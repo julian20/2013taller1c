1766e60d4c31220df4a1d70525db246807ba0d26
tomasboccardo@gmail.com <tomasboccardo@gmail.com@32cccb14-7060-b6e5-34ce-36fad607224b>
2013-03-23 15:15:33 +0000

    Tomi: Ahora compila. Pequeño error de definicion de la funcion run arreglado.

--- trunk/TpTaller/Debug/src/main.d
+++
@@ -1,7 +0,0 @@
-src/main.d: ../src/main.cpp \
- /home/tomas/workspace/TpTaller/includes/model/ConfigurationReader.h \
- /home/tomas/workspace/TpTaller/includes/model/Game.h
-
-/home/tomas/workspace/TpTaller/includes/model/ConfigurationReader.h:
-
-/home/tomas/workspace/TpTaller/includes/model/Game.h:
--- trunk/TpTaller/Debug/src/model/ConfigurationReader.d
+++
@@ -1,4 +0,0 @@
-src/model/ConfigurationReader.d: ../src/model/ConfigurationReader.cpp \
- /home/tomas/workspace/TpTaller/includes/model/ConfigurationReader.h
-
-/home/tomas/workspace/TpTaller/includes/model/ConfigurationReader.h:
