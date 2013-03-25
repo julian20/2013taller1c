3dae74a08d9a78be612cf8fd378d2d229551c9c2
lucas.006 <lucas.006@live.com.ar>
2013-03-25 14:01:11 +0000

    Lucas: Agrego tests independientes para mapa

---
+++ TpTaller/tests/mapTests/mapTests.cpp
@@ -0,0 +1,27 @@
+/*
+ * mapaTests.cpp
+ *
+ *  Created on: 24/03/2013
+ *      Author: Lucas
+ */
+ 
+#include <stdio.h>
+#include <Game.h>
+
+using namespace std;
+
+
+
+int main( int argc, char* args[] )
+{
+ 
+	Game* game = new Game(NULL);
+	
+	
+	game->run();
+	
+	game->~Game();
+	delete game;
+    return 0;
+}
+
