ce79017a69a6618f1cbb7a0480b0a98cadac8cbd
gonchu.b <gonchu.b@gmail.com>
2013-04-01 00:49:15 +0000

    Gonchu: optimizo el parser. Elimino estructuras auxiliares. Solo me tengo que poner de acuerdo con fede como va a tomar los datos del mapa para ver que carajo hago, porque si sigo asi va a haber informacion duplicada.

--- TpTaller/src/view/map/Map.cpp
+++ TpTaller/src/view/map/Map.cpp
@@ -6,46 +6,47 @@
 #define TilesOverlap	2		// px
 #define TilesScale  	1
 #define CameraSpeed		5		// px
-
 Map::Map(MapData* _data) {
-    data = _data;
-    cameraX = cameraY = 0;
-    mapController = new MapController();
+	data = _data;
+	cameraX = cameraY = 0;
+	mapController = new MapController();
 
-    DefineTexturePaths();
-    GraphicalSetup();
+	DefineTexturePaths();
+	GraphicalSetup();
 }
 
 void Map::DefineTexturePaths() {
-    texturesPaths[ MapData::NEUTRAL ] = "texturesTiles/sand.png";
-    texturesPaths[ MapData::SOIL ] = "texturesTiles/dirtTile.png";
-    texturesPaths[ MapData::WATER ] = "texturesTiles/Water.png";
-    texturesPaths[ MapData::TREE ] = "texturesTiles/stonebrick.png";
+	texturesPaths[MapData::NEUTRAL] = "texturesTiles/sand.png";
+	texturesPaths[MapData::SOIL] = "texturesTiles/dirtTile.png";
+	texturesPaths[MapData::WATER] = "texturesTiles/Water.png";
+	texturesPaths[MapData::TREE] = "texturesTiles/stonebrick.png";
 
-    backgroundPath = "./background.png";
+	backgroundPath = "./background.png";
 }
 
-Vector2* Map::GetCamera(){
+Vector2* Map::GetCamera() {
 	return new Vector2(cameraX, cameraY);
 }
 
 void Map::GraphicalSetup() {
-    //Cargamos las textura de los tiles
-    for (int i = 0; i < MapData::AMOUNT_TYPES; i++) {
-        tilesTextures[i] = IMG_Load(texturesPaths[i].c_str());
-        if (tilesTextures[i] == NULL) {
-            printf("Unable to load tile texture n°:%d: %s\n", i, SDL_GetError());
-            exit(1);
-        }
-    }
+	//Cargamos las textura de los tiles
+	for (int i = 0; i < MapData::AMOUNT_TYPES; i++) {
+		tilesTextures[i] = IMG_Load(texturesPaths[i].c_str());
+		if (tilesTextures[i] == NULL) {
+			printf("Unable to load tile texture n°:%d: %s\n", i,
+					SDL_GetError());
+			exit(1);
+		}
+	}
 
-    // Convertimos las imagenes a display format para que 
-    // se muestre mas rapido por pantalla
+	// Convertimos las imagenes a display format para que
+	// se muestre mas rapido por pantalla
 
-    for (int i = 0; i < MapData::AMOUNT_TYPES; i++) {
-        tilesTextures[i] = rotozoomSurfaceXY(tilesTextures[i], 0, TilesScale, TilesScale, 0);
-        tilesTextures[i] = SDL_DisplayFormatAlpha(tilesTextures[i]);
-    }
+	for (int i = 0; i < MapData::AMOUNT_TYPES; i++) {
+		tilesTextures[i] = rotozoomSurfaceXY(tilesTextures[i], 0, TilesScale,
+				TilesScale, 0);
+		tilesTextures[i] = SDL_DisplayFormatAlpha(tilesTextures[i]);
+	}
 
 }
 
@@ -76,21 +77,21 @@ void Map::CameraUpdate() {
 	}
 }
 
-void Map::SetUpPersonajes(){
+void Map::SetUpPersonajes() {
 	// Setea la posicion por unica vez de los personajes (y en un futuro posiblemente
 	// todo lo representable) en el mapa.
-    SDL_Rect posTile;
+	SDL_Rect posTile;
 
-    for (int col = 0; col < data->GetNCols(); col++) {
-        for (int row = 0; row < data->GetNRows(); row++) {
+	for (int col = 0; col < data->GetNCols(); col++) {
+		for (int row = 0; row < data->GetNRows(); row++) {
 
-            Personaje* personaje = data->GetPersonaje(row, col);
-            if (personaje != NULL) {
-            	posTile = TILE_POS_FUNC(row, col);
-            	personaje->SetPos((float)posTile.x, (float)posTile.y);
-            }
-        }
-    }
+			Personaje* personaje = data->GetPersonaje(row, col);
+			if (personaje != NULL) {
+				posTile = TILE_POS_FUNC(row, col);
+				personaje->SetPos((float) posTile.x, (float) posTile.y);
+			}
+		}
+	}
 }
 
 void Map::Draw(SDL_Surface* screen) {
@@ -98,75 +99,74 @@ void Map::Draw(SDL_Surface* screen) {
 	CameraUpdate();
 
 	//Personaje* personajes = NULL;
-    SDL_Rect posTile;
+	SDL_Rect posTile;
 
-    for (int col = 0; col < data->GetNCols(); col++) {
+	for (int col = 0; col < data->GetNCols(); col++) {
 
-        for (int row = 0; row < data->GetNRows(); row++) {
+		for (int row = 0; row < data->GetNRows(); row++) {
 
-        	posTile = TILE_POS_FUNC(row, col);
+			posTile = TILE_POS_FUNC(row, col);
 
-            SDL_BlitSurface(tilesTextures[ data->GetTileType(row, col) ],
-            				NULL,
-            				screen,
-            				&posTile);
+			SDL_BlitSurface(tilesTextures[data->GetTileType(row, col)], NULL,
+					screen, &posTile);
 
-            tileData* tileData = data->GetTileData(row, col);
+			tileData* tileData = data->GetTileData(row, col);
 
-            // TODO: Dibujar las cosas de tileData
-            // En esta parte deberia agregarlo a una lista, como solo hay uno por
-            // ahora solo se guarda en una variable
+			// TODO: Dibujar las cosas de tileData
+			// En esta parte deberia agregarlo a una lista, como solo hay uno por
+			// ahora solo se guarda en una variable
 
-           // Personaje* personaje = data->GetPersonaje(row, col);
-           // if (personaje != NULL) personajes = personaje;
-        }
-    }
+			// Personaje* personaje = data->GetPersonaje(row, col);
+			// if (personaje != NULL) personajes = personaje;
+		}
+	}
 
-    // Luego se blitean todos los personajes despues de haber bliteado el piso
-    // para que el piso no tape a los flacos.
-  /*  if (personajes != NULL){
-		PersonajeVista* personajeVista =
-				new PersonajeVista(personajes, "resources/foo.png", screen);
+	// Luego se blitean todos los personajes despues de haber bliteado el piso
+	// para que el piso no tape a los flacos.
+	/*  if (personajes != NULL){
+	 PersonajeVista* personajeVista =
+	 new PersonajeVista(personajes, "resources/foo.png", screen);
 
-		personajeVista->UpdateCameraPos(cameraX, cameraY);
-		personajeVista->Mostrar();
-    }*/
+	 personajeVista->UpdateCameraPos(cameraX, cameraY);
+	 personajeVista->Mostrar();
+	 }*/
 }
 
 SDL_Rect Map::GetSquaredMapTilePos(int row, int col) {
-    int widthTexture = tilesTextures[0]->w - TilesOverlap;
-    int heightTexture = tilesTextures[0]->h - TilesOverlap;
+	int widthTexture = tilesTextures[0]->w - TilesOverlap;
+	int heightTexture = tilesTextures[0]->h - TilesOverlap;
 
-    SDL_Rect posTile;
+	SDL_Rect posTile;
 
-    posTile.x = cameraX - MapMargin + (row % 2) * widthTexture / 2 + col * widthTexture;
-    posTile.y = cameraY - MapMargin + row * heightTexture / 2;
-    posTile.w = widthTexture;
-    posTile.h = heightTexture;
+	posTile.x = cameraX - MapMargin + (row % 2) * widthTexture / 2
+			+ col * widthTexture;
+	posTile.y = cameraY - MapMargin + row * heightTexture / 2;
+	posTile.w = widthTexture;
+	posTile.h = heightTexture;
 
-    return posTile;
+	return posTile;
 }
 
 SDL_Rect Map::GetDiamondShapeMapTilePos(int row, int col) {
-    int widthTexture = tilesTextures[0]->w - TilesOverlap;
-    int heightTexture = tilesTextures[0]->h - TilesOverlap;
+	int widthTexture = tilesTextures[0]->w - TilesOverlap;
+	int heightTexture = tilesTextures[0]->h - TilesOverlap;
 
-    SDL_Rect posTile;
+	SDL_Rect posTile;
 
-    posTile.x = cameraX + (col + row) * widthTexture / 2;
-    posTile.y = cameraY + (col - row) * heightTexture / 2;
-    posTile.w = widthTexture;
-    posTile.h = heightTexture;
+	posTile.x = cameraX + (col + row) * widthTexture / 2;
+	posTile.y = cameraY + (col - row) * heightTexture / 2;
+	posTile.w = widthTexture;
+	posTile.h = heightTexture;
 
-    return posTile;
+	return posTile;
 }
 
 void Map::ClickOn(int x, int y, int button) {
 	// Selecciona la casilla mas o menos bien, idealizandola como un cuadrado.
 	SDL_Rect firstTile = TILE_POS_FUNC(0, 0);
 
-	int row = (y - firstTile.y)*2/firstTile.h;
-	int col = (x - firstTile.x)/firstTile.w;
+	int row = (y - firstTile.y) * 2 / firstTile.h;
+	int col = (x - firstTile.x) / firstTile.w;
 
 	printf("row: %d, col: %d\n", row, col);
 }
@@ -174,6 +174,4 @@ void Map::ClickOn(int x, int y, int button) {
 void Map::Update() {
 
 }
-
-
 
--- TpTaller/src/view/personaje/PersonajeVista.cpp
+++ TpTaller/src/view/personaje/PersonajeVista.cpp
@@ -18,278 +18,287 @@
 
 //#define SCALE				0.2
 
-
 //como es animado necesito la cantidad de clips por lado.
-int NUMERODECLIPS=4;
+int NUMERODECLIPS = 4;
 
-SDL_Rect clipsDerecha[ 4 ];
-SDL_Rect clipsIzquierda[ 4 ];
-SDL_Rect clipsArriba[ 4 ];
-SDL_Rect clipsAbajo[ 4 ];
-SDL_Rect clipsAbajoIzq[ 4 ];
-SDL_Rect clipsAbajoDer[ 4 ];
-SDL_Rect clipsArribaDer[ 4 ];
-SDL_Rect clipsArribaIzq[ 4 ];
+SDL_Rect clipsDerecha[4];
+SDL_Rect clipsIzquierda[4];
+SDL_Rect clipsArriba[4];
+SDL_Rect clipsAbajo[4];
+SDL_Rect clipsAbajoIzq[4];
+SDL_Rect clipsAbajoDer[4];
+SDL_Rect clipsArribaDer[4];
+SDL_Rect clipsArribaIzq[4];
 
-SDL_Surface *load_image( char* urlImagen )
-{
-    //The image that's loaded
-    SDL_Surface* loadedImage = NULL;
+SDL_Surface *load_image(char* urlImagen) {
+	//The image that's loaded
+	SDL_Surface* loadedImage = NULL;
 
-    //The optimized surface that will be used
-    SDL_Surface* optimizedImage = NULL;
+	//The optimized surface that will be used
+	SDL_Surface* optimizedImage = NULL;
 
-    loadedImage = IMG_Load( urlImagen );
+	loadedImage = IMG_Load(urlImagen);
 
-    if( loadedImage != NULL )
-    {
-    	//loadedImage = rotozoomSurfaceXY(loadedImage, 0, SCALE, SCALE, 0);
+	if (loadedImage != NULL) {
+		//loadedImage = rotozoomSurfaceXY(loadedImage, 0, SCALE, SCALE, 0);
 
-        //Create an optimized surface
-        optimizedImage = SDL_DisplayFormatAlpha( loadedImage );
+		//Create an optimized surface
+		optimizedImage = SDL_DisplayFormatAlpha(loadedImage);
 
-        SDL_FreeSurface( loadedImage );
+		SDL_FreeSurface(loadedImage);
 
-        if( optimizedImage != NULL )
-        {
-            //Color key surface
-            SDL_SetColorKey( optimizedImage, SDL_SRCCOLORKEY, SDL_MapRGB( optimizedImage->format, 0, 0xFF, 0xFF ) );
-        }
-    }
+		if (optimizedImage != NULL) {
+			//Color key surface
+			SDL_SetColorKey(optimizedImage, SDL_SRCCOLORKEY,
+					SDL_MapRGB(optimizedImage->format, 0, 0xFF, 0xFF));
+		}
+	}
 
-    return optimizedImage;
+	return optimizedImage;
 }
 
-void PersonajeVista::Draw( float x, float y, SDL_Surface* source, SDL_Surface* screen, SDL_Rect* clip )
-{
-    SDL_Rect offset;
+void PersonajeVista::Draw(float x, float y, SDL_Surface* source,
+		SDL_Surface* screen, SDL_Rect* clip) {
+	SDL_Rect offset;
 
-    //offset.x = (int)x + cameraX - (clip->w/SCALE)/2;
-    offset.x= (int)x +cameraX -clip->w;
-    offset.y = (int)y + cameraY- clip->h;
-    offset.w = clip->w;
-    offset.h = clip->h;
-    SDL_BlitSurface( source, clip, screen, &offset );
+	//offset.x = (int)x + cameraX - (clip->w/SCALE)/2;
+	offset.x = (int) x + cameraX - clip->w;
+	offset.y = (int) y + cameraY - clip->h;
+	offset.w = clip->w;
+	offset.h = clip->h;
+	SDL_BlitSurface(source, clip, screen, &offset);
 }
 
-void PersonajeVista::UpdateCameraPos(int x, int y){
+void PersonajeVista::UpdateCameraPos(int x, int y) {
 	cameraX = x;
 	cameraY = y;
 }
 
-PersonajeVista::PersonajeVista(Personaje* unPersonaje, char* urlImagen)
-{
+PersonajeVista::PersonajeVista(Personaje* unPersonaje, char* urlImagen) {
 	cameraX = cameraY = 0;
 	marco = 0;
 	estado = 0;
 	/* TODO: Faltaria lanzar una excepcion en caso de que no recieba alguno de los parametros
-     if(!unPersonaje)
-    {
-        throw new FaltaParametroException("PersonajeVista");
-    }*/
-    
-    miPersonaje = unPersonaje;
+	 if(!unPersonaje)
+	 {
+	 throw new FaltaParametroException("PersonajeVista");
+	 }*/
 
-   // try
-    //{
-    	//this->fondo = fondo;
-        personajeImagen = this->CargarImagen(urlImagen);
-        this->EstablecerLosClips(NUMERODECLIPS);
-   /* }catch(ERROR e)
-    {
-        //TODO:cargo una imagen alternativa.
-    }*/
+	miPersonaje = unPersonaje;
+
+	// try
+	//{
+	//this->fondo = fondo;
+	personajeImagen = this->CargarImagen(urlImagen);
+	this->EstablecerLosClips(NUMERODECLIPS);
+	/* }catch(ERROR e)
+	 {
+	 //TODO:cargo una imagen alternativa.
+	 }*/
 }
 
-SDL_Surface* PersonajeVista::CargarImagen(char* img)
-{
-    //Load the sprite sheet
-    if(!img)
-    {
-      //  throw new FaltaParametroException();
-    }
-    
-    SDL_Surface* miPersonajeImagen = load_image( img );
+SDL_Surface* PersonajeVista::CargarImagen(char* img) {
+	//Load the sprite sheet
+	if (!img) {
+		//  throw new FaltaParametroException();
+	}
 
-    //If there was a problem in loading the sprite
-    if( !miPersonajeImagen)
-    {
-    	printf("NO SE HA ENCONTRADO LA IMAGEN\n");
-        //return false;
-        //TODO: cargo una alternativa
-    }
+	SDL_Surface* miPersonajeImagen = load_image(img);
 
-    marco = 0;
-    estado = PERSONAJE_DERECHA;
-    return miPersonajeImagen;
+	//If there was a problem in loading the sprite
+	if (!miPersonajeImagen) {
+		printf("NO SE HA ENCONTRADO LA IMAGEN\n");
+		//return false;
+		//TODO: cargo una alternativa
+	}
+
+	marco = 0;
+	estado = PERSONAJE_DERECHA;
+	return miPersonajeImagen;
 }
 
-void PersonajeVista::Mostrar(SDL_Surface* fondo)
-{
+void PersonajeVista::Mostrar(SDL_Surface* fondo) {
 	Vector2* movementDirection = this->miPersonaje->GetMovementDirection();
 	float direction = movementDirection->GetAngle();
 	Vector2* pos = this->miPersonaje->GetCurrentPos();
 
-    int x = pos->GetX() + OFFSET_X;
-    int y = pos->GetY() + OFFSET_Y;
+	int x = pos->GetX() + OFFSET_X;
+	int y = pos->GetY() + OFFSET_Y;
 
-    SDL_Rect* clipToDraw;
+	SDL_Rect* clipToDraw;
 
-	if (M_PI * 15 / 8 < direction || direction < M_PI * 1 / 8) 		clipToDraw = &clipsDerecha[ marco ];
-	else if (M_PI * 1 / 8 < direction && direction < M_PI * 3 / 8) 	clipToDraw = &clipsAbajoDer[ marco ];
-	else if (M_PI * 3 / 8 < direction && direction < M_PI * 5 / 8) 	clipToDraw = &clipsAbajo[ marco ];
-	else if (M_PI * 5 / 8 < direction && direction < M_PI * 7 / 8) 	clipToDraw = &clipsAbajoIzq[ marco ];
-	else if (M_PI * 7 / 8 < direction && direction < M_PI * 9 / 8) 	clipToDraw = &clipsIzquierda[ marco ];
-	else if (M_PI * 9 / 8 < direction && direction < M_PI * 11 / 8) clipToDraw = &clipsArribaIzq[ marco ];
-	else if (M_PI * 11 / 8 < direction && direction < M_PI * 13 / 8)clipToDraw = &clipsArriba[ marco ];
-	else if (M_PI * 13 / 8 < direction && direction < M_PI * 15 / 8)clipToDraw = &clipsArribaDer[ marco ];
+	if (M_PI * 15 / 8 < direction || direction < M_PI * 1 / 8)
+		clipToDraw = &clipsDerecha[marco];
+	else if (M_PI * 1 / 8 < direction && direction < M_PI * 3 / 8)
+		clipToDraw = &clipsAbajoDer[marco];
+	else if (M_PI * 3 / 8 < direction && direction < M_PI * 5 / 8)
+		clipToDraw = &clipsAbajo[marco];
+	else if (M_PI * 5 / 8 < direction && direction < M_PI * 7 / 8)
+		clipToDraw = &clipsAbajoIzq[marco];
+	else if (M_PI * 7 / 8 < direction && direction < M_PI * 9 / 8)
+		clipToDraw = &clipsIzquierda[marco];
+	else if (M_PI * 9 / 8 < direction && direction < M_PI * 11 / 8)
+		clipToDraw = &clipsArribaIzq[marco];
+	else if (M_PI * 11 / 8 < direction && direction < M_PI * 13 / 8)
+		clipToDraw = &clipsArriba[marco];
+	else if (M_PI * 13 / 8 < direction && direction < M_PI * 15 / 8)
+		clipToDraw = &clipsArribaDer[marco];
 
-	if (miPersonaje->IsMoving()) this->marco++;	// Move to the next marco in the animation
-	else	marco = 0;	// Stop Animation, unit standing.
+	if (miPersonaje->IsMoving())
+		this->marco++;	// Move to the next marco in the animation
+	else
+		marco = 0;	// Stop Animation, unit standing.
 
-    if( marco >= 4 ) marco = 0;    // Loop the animation
+	if (marco >= 4)
+		marco = 0;    // Loop the animation
 
-    Draw(x, y, this->personajeImagen, fondo, clipToDraw );
+	Draw(x, y, this->personajeImagen, fondo, clipToDraw);
 }
-void PersonajeVista::EstablecerLosClips(int cantidadPorLado)
-{
-	for(int cantidadDePosiciones=0;cantidadDePosiciones<4;cantidadDePosiciones++)
-	{
-		clipsDerecha[ cantidadDePosiciones ].x = PERSONAJE_ANCHO*cantidadDePosiciones;
-		clipsDerecha[ cantidadDePosiciones ].y = 0;
-		clipsDerecha[ cantidadDePosiciones ].w = PERSONAJE_ANCHO;
-		clipsDerecha[ cantidadDePosiciones ].h = PERSONAJE_ALTO;
+void PersonajeVista::EstablecerLosClips(int cantidadPorLado) {
+	for (int cantidadDePosiciones = 0; cantidadDePosiciones < 4;
+			cantidadDePosiciones++) {
+		clipsDerecha[cantidadDePosiciones].x = PERSONAJE_ANCHO
+				* cantidadDePosiciones;
+		clipsDerecha[cantidadDePosiciones].y = 0;
+		clipsDerecha[cantidadDePosiciones].w = PERSONAJE_ANCHO;
+		clipsDerecha[cantidadDePosiciones].h = PERSONAJE_ALTO;
 
-		 clipsIzquierda[ cantidadDePosiciones ].x = PERSONAJE_ANCHO*cantidadDePosiciones;
-		 clipsIzquierda[ cantidadDePosiciones ].y = PERSONAJE_ALTO;
-		 clipsIzquierda[ cantidadDePosiciones ].w = PERSONAJE_ANCHO;
-		 clipsIzquierda[ cantidadDePosiciones ].h = PERSONAJE_ALTO;
+		clipsIzquierda[cantidadDePosiciones].x = PERSONAJE_ANCHO
+				* cantidadDePosiciones;
+		clipsIzquierda[cantidadDePosiciones].y = PERSONAJE_ALTO;
+		clipsIzquierda[cantidadDePosiciones].w = PERSONAJE_ANCHO;
+		clipsIzquierda[cantidadDePosiciones].h = PERSONAJE_ALTO;
 
-		 clipsArriba[ cantidadDePosiciones ].x = PERSONAJE_ANCHO*cantidadDePosiciones;
-		 clipsArriba[ cantidadDePosiciones ].y = PERSONAJE_ALTO * 2;
-		 clipsArriba[ cantidadDePosiciones ].w = PERSONAJE_ANCHO;
-		 clipsArriba[ cantidadDePosiciones ].h = PERSONAJE_ALTO;
+		clipsArriba[cantidadDePosiciones].x = PERSONAJE_ANCHO
+				* cantidadDePosiciones;
+		clipsArriba[cantidadDePosiciones].y = PERSONAJE_ALTO * 2;
+		clipsArriba[cantidadDePosiciones].w = PERSONAJE_ANCHO;
+		clipsArriba[cantidadDePosiciones].h = PERSONAJE_ALTO;
 
-		 clipsAbajo[ cantidadDePosiciones ].x = PERSONAJE_ANCHO*cantidadDePosiciones;
-		 clipsAbajo[ cantidadDePosiciones ].y = PERSONAJE_ALTO * 3;
-		 clipsAbajo[ cantidadDePosiciones ].w = PERSONAJE_ANCHO;
-		 clipsAbajo[ cantidadDePosiciones ].h = PERSONAJE_ALTO;
+		clipsAbajo[cantidadDePosiciones].x = PERSONAJE_ANCHO
+				* cantidadDePosiciones;
+		clipsAbajo[cantidadDePosiciones].y = PERSONAJE_ALTO * 3;
+		clipsAbajo[cantidadDePosiciones].w = PERSONAJE_ANCHO;
+		clipsAbajo[cantidadDePosiciones].h = PERSONAJE_ALTO;
 
-		 clipsArribaIzq[ cantidadDePosiciones ].x = PERSONAJE_ANCHO*cantidadDePosiciones;
-		 clipsArribaIzq[ cantidadDePosiciones ].y = PERSONAJE_ALTO * 6;
-		 clipsArribaIzq[ cantidadDePosiciones ].w = PERSONAJE_ANCHO;
-		 clipsArribaIzq[ cantidadDePosiciones ].h = PERSONAJE_ALTO;
+		clipsArribaIzq[cantidadDePosiciones].x = PERSONAJE_ANCHO
+				* cantidadDePosiciones;
+		clipsArribaIzq[cantidadDePosiciones].y = PERSONAJE_ALTO * 6;
+		clipsArribaIzq[cantidadDePosiciones].w = PERSONAJE_ANCHO;
+		clipsArribaIzq[cantidadDePosiciones].h = PERSONAJE_ALTO;
 
-		 clipsArribaDer[ cantidadDePosiciones ].x = PERSONAJE_ANCHO*cantidadDePosiciones;
-		 clipsArribaDer[ cantidadDePosiciones ].y = PERSONAJE_ALTO * 7;
-		 clipsArribaDer[ cantidadDePosiciones ].w = PERSONAJE_ANCHO;
-		 clipsArribaDer[ cantidadDePosiciones ].h = PERSONAJE_ALTO;
+		clipsArribaDer[cantidadDePosiciones].x = PERSONAJE_ANCHO
+				* cantidadDePosiciones;
+		clipsArribaDer[cantidadDePosiciones].y = PERSONAJE_ALTO * 7;
+		clipsArribaDer[cantidadDePosiciones].w = PERSONAJE_ANCHO;
+		clipsArribaDer[cantidadDePosiciones].h = PERSONAJE_ALTO;
 
-		 clipsAbajoIzq[ cantidadDePosiciones ].x = PERSONAJE_ANCHO*cantidadDePosiciones;
-		 clipsAbajoIzq[ cantidadDePosiciones ].y = PERSONAJE_ALTO * 5;
-		 clipsAbajoIzq[ cantidadDePosiciones ].w = PERSONAJE_ANCHO;
-		 clipsAbajoIzq[ cantidadDePosiciones ].h = PERSONAJE_ALTO;
+		clipsAbajoIzq[cantidadDePosiciones].x = PERSONAJE_ANCHO
+				* cantidadDePosiciones;
+		clipsAbajoIzq[cantidadDePosiciones].y = PERSONAJE_ALTO * 5;
+		clipsAbajoIzq[cantidadDePosiciones].w = PERSONAJE_ANCHO;
+		clipsAbajoIzq[cantidadDePosiciones].h = PERSONAJE_ALTO;
 
-		 clipsAbajoDer[ cantidadDePosiciones ].x = PERSONAJE_ANCHO*cantidadDePosiciones;
-		 clipsAbajoDer[ cantidadDePosiciones ].y = PERSONAJE_ALTO * 4;
-		 clipsAbajoDer[ cantidadDePosiciones ].w = PERSONAJE_ANCHO;
-		 clipsAbajoDer[ cantidadDePosiciones ].h = PERSONAJE_ALTO;
+		clipsAbajoDer[cantidadDePosiciones].x = PERSONAJE_ANCHO
+				* cantidadDePosiciones;
+		clipsAbajoDer[cantidadDePosiciones].y = PERSONAJE_ALTO * 4;
+		clipsAbajoDer[cantidadDePosiciones].w = PERSONAJE_ANCHO;
+		clipsAbajoDer[cantidadDePosiciones].h = PERSONAJE_ALTO;
 
 	}
 }
 /*
-void PersonajeVista::EstablecerLosClips(int cantidadPorLado)
-{
-    //Clip the sprites
-    clipsDerecha[ 0 ].x = 0;
-    clipsDerecha[ 0 ].y = 0;
-    clipsDerecha[ 0 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsDerecha[ 0 ].h = PERSONAJE_ALTO*SCALE;
+ void PersonajeVista::EstablecerLosClips(int cantidadPorLado)
+ {
+ //Clip the sprites
+ clipsDerecha[ 0 ].x = 0;
+ clipsDerecha[ 0 ].y = 0;
+ clipsDerecha[ 0 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsDerecha[ 0 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsDerecha[ 1 ].x = PERSONAJE_ANCHO*SCALE;
-    clipsDerecha[ 1 ].y = 0;
-    clipsDerecha[ 1 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsDerecha[ 1 ].h = PERSONAJE_ALTO*SCALE;
+ clipsDerecha[ 1 ].x = PERSONAJE_ANCHO*SCALE;
+ clipsDerecha[ 1 ].y = 0;
+ clipsDerecha[ 1 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsDerecha[ 1 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsDerecha[ 2 ].x = PERSONAJE_ANCHO * 2*SCALE;
-    clipsDerecha[ 2 ].y = 0;
-    clipsDerecha[ 2 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsDerecha[ 2 ].h = PERSONAJE_ALTO*SCALE;
+ clipsDerecha[ 2 ].x = PERSONAJE_ANCHO * 2*SCALE;
+ clipsDerecha[ 2 ].y = 0;
+ clipsDerecha[ 2 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsDerecha[ 2 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsDerecha[ 3 ].x = PERSONAJE_ANCHO * 3*SCALE;
-    clipsDerecha[ 3 ].y = 0;
-    clipsDerecha[ 3 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsDerecha[ 3 ].h = PERSONAJE_ALTO*SCALE;
+ clipsDerecha[ 3 ].x = PERSONAJE_ANCHO * 3*SCALE;
+ clipsDerecha[ 3 ].y = 0;
+ clipsDerecha[ 3 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsDerecha[ 3 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsIzquierda[ 0 ].x = 0;
-    clipsIzquierda[ 0 ].y = PERSONAJE_ALTO*SCALE;
-    clipsIzquierda[ 0 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsIzquierda[ 0 ].h = PERSONAJE_ALTO*SCALE;
+ clipsIzquierda[ 0 ].x = 0;
+ clipsIzquierda[ 0 ].y = PERSONAJE_ALTO*SCALE;
+ clipsIzquierda[ 0 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsIzquierda[ 0 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsIzquierda[ 1 ].x = PERSONAJE_ANCHO*SCALE;
-    clipsIzquierda[ 1 ].y = PERSONAJE_ALTO*SCALE;
-    clipsIzquierda[ 1 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsIzquierda[ 1 ].h = PERSONAJE_ALTO*SCALE;
+ clipsIzquierda[ 1 ].x = PERSONAJE_ANCHO*SCALE;
+ clipsIzquierda[ 1 ].y = PERSONAJE_ALTO*SCALE;
+ clipsIzquierda[ 1 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsIzquierda[ 1 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsIzquierda[ 2 ].x = PERSONAJE_ANCHO * 2*SCALE;
-    clipsIzquierda[ 2 ].y = PERSONAJE_ALTO*SCALE;
-    clipsIzquierda[ 2 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsIzquierda[ 2 ].h = PERSONAJE_ALTO*SCALE;
+ clipsIzquierda[ 2 ].x = PERSONAJE_ANCHO * 2*SCALE;
+ clipsIzquierda[ 2 ].y = PERSONAJE_ALTO*SCALE;
+ clipsIzquierda[ 2 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsIzquierda[ 2 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsIzquierda[ 3 ].x = PERSONAJE_ANCHO * 3*SCALE;
-    clipsIzquierda[ 3 ].y = PERSONAJE_ALTO*SCALE;
-    clipsIzquierda[ 3 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsIzquierda[ 3 ].h = PERSONAJE_ALTO*SCALE;
-    
-    clipsArriba[ 0 ].x = 0;
-    clipsArriba[ 0 ].y = PERSONAJE_ALTO * 2*SCALE;
-    clipsArriba[ 0 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsArriba[ 0 ].h = PERSONAJE_ALTO*SCALE;
+ clipsIzquierda[ 3 ].x = PERSONAJE_ANCHO * 3*SCALE;
+ clipsIzquierda[ 3 ].y = PERSONAJE_ALTO*SCALE;
+ clipsIzquierda[ 3 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsIzquierda[ 3 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsArriba[ 1 ].x = PERSONAJE_ANCHO*SCALE;
-    clipsArriba[ 1 ].y = PERSONAJE_ALTO * 2*SCALE;
-    clipsArriba[ 1 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsArriba[ 1 ].h = PERSONAJE_ALTO*SCALE;
+ clipsArriba[ 0 ].x = 0;
+ clipsArriba[ 0 ].y = PERSONAJE_ALTO * 2*SCALE;
+ clipsArriba[ 0 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsArriba[ 0 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsArriba[ 2 ].x = PERSONAJE_ANCHO * 2*SCALE;
-    clipsArriba[ 2 ].y = PERSONAJE_ALTO * 2*SCALE;
-    clipsArriba[ 2 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsArriba[ 2 ].h = PERSONAJE_ALTO*SCALE;
+ clipsArriba[ 1 ].x = PERSONAJE_ANCHO*SCALE;
+ clipsArriba[ 1 ].y = PERSONAJE_ALTO * 2*SCALE;
+ clipsArriba[ 1 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsArriba[ 1 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsArriba[ 3 ].x = PERSONAJE_ANCHO * 3*SCALE;
-    clipsArriba[ 3 ].y = PERSONAJE_ALTO * 2*SCALE;
-    clipsArriba[ 3 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsArriba[ 3 ].h = PERSONAJE_ALTO*SCALE;
+ clipsArriba[ 2 ].x = PERSONAJE_ANCHO * 2*SCALE;
+ clipsArriba[ 2 ].y = PERSONAJE_ALTO * 2*SCALE;
+ clipsArriba[ 2 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsArriba[ 2 ].h = PERSONAJE_ALTO*SCALE;
 
-    
-    clipsAbajo[ 0 ].x = 0;
-    clipsAbajo[ 0 ].y = PERSONAJE_ALTO * 3*SCALE;
-    clipsAbajo[ 0 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsAbajo[ 0 ].h = PERSONAJE_ALTO*SCALE;
+ clipsArriba[ 3 ].x = PERSONAJE_ANCHO * 3*SCALE;
+ clipsArriba[ 3 ].y = PERSONAJE_ALTO * 2*SCALE;
+ clipsArriba[ 3 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsArriba[ 3 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsAbajo[ 1 ].x = PERSONAJE_ANCHO*SCALE;
-    clipsAbajo[ 1 ].y = PERSONAJE_ALTO * 3*SCALE;
-    clipsAbajo[ 1 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsAbajo[ 1 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsAbajo[ 2 ].x = PERSONAJE_ANCHO * 2*SCALE;
-    clipsAbajo[ 2 ].y = PERSONAJE_ALTO * 3*SCALE;
-    clipsAbajo[ 2 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsAbajo[ 2 ].h = PERSONAJE_ALTO*SCALE;
+ clipsAbajo[ 0 ].x = 0;
+ clipsAbajo[ 0 ].y = PERSONAJE_ALTO * 3*SCALE;
+ clipsAbajo[ 0 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsAbajo[ 0 ].h = PERSONAJE_ALTO*SCALE;
 
-    clipsAbajo[ 3 ].x = PERSONAJE_ANCHO * 3*SCALE;
-    clipsAbajo[ 3 ].y = PERSONAJE_ALTO * 3*SCALE;
-    clipsAbajo[ 3 ].w = PERSONAJE_ANCHO*SCALE;
-    clipsAbajo[ 3 ].h = PERSONAJE_ALTO*SCALE;
+ clipsAbajo[ 1 ].x = PERSONAJE_ANCHO*SCALE;
+ clipsAbajo[ 1 ].y = PERSONAJE_ALTO * 3*SCALE;
+ clipsAbajo[ 1 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsAbajo[ 1 ].h = PERSONAJE_ALTO*SCALE;
 
-}*/
+ clipsAbajo[ 2 ].x = PERSONAJE_ANCHO * 2*SCALE;
+ clipsAbajo[ 2 ].y = PERSONAJE_ALTO * 3*SCALE;
+ clipsAbajo[ 2 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsAbajo[ 2 ].h = PERSONAJE_ALTO*SCALE;
 
-PersonajeVista::~PersonajeVista() 
-{
-    //libera la memoria que pide para La imagen
-    SDL_FreeSurface( this->personajeImagen );
+ clipsAbajo[ 3 ].x = PERSONAJE_ANCHO * 3*SCALE;
+ clipsAbajo[ 3 ].y = PERSONAJE_ALTO * 3*SCALE;
+ clipsAbajo[ 3 ].w = PERSONAJE_ANCHO*SCALE;
+ clipsAbajo[ 3 ].h = PERSONAJE_ALTO*SCALE;
 
-    //Quita SDL
-    SDL_Quit();
+ }*/
+
+PersonajeVista::~PersonajeVista() {
+	//libera la memoria que pide para La imagen
+	SDL_FreeSurface(this->personajeImagen);
+
+	//Quita SDL
+	SDL_Quit();
 }
 
