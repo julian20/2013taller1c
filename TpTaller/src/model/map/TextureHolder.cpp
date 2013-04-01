bc59ad18626414854917e9f13783ea2db4708033
gonchu.b <gonchu.b@gmail.com>
2013-04-01 04:26:33 +0000

    Gonchu: agrego un "manejador" de texturas, al que le pedimos una textura por su "nombre" y este se encarga de buscarla en disco, cargarla y manejar lo que sucede si no la encuentra.

---
+++ TpTaller/src/model/map/TextureHolder.cpp
@@ -0,0 +1,50 @@
+/*
+ * TextureHolder.cpp
+ *
+ *  Created on: Apr 1, 2013
+ *      Author: gonchub
+ */
+
+#include <model/map/TextureHolder.h>
+
+#define DEFAULT_TEXTURE_ID "default"
+
+TextureHolder::TextureHolder() {
+}
+
+TextureHolder::~TextureHolder() {
+	for (unsigned int i = 0; i < listOfTextures.size(); i++) {
+		delete listOfTextures[i];
+	}
+}
+
+bool TextureHolder::duplicateTexture(std::string id) {
+	for (unsigned int i = 0; i < this->listOfTextures.size(); i++) {
+		TileDefinition* auxTileDefinition = this->listOfTextures[i];
+		if (id.compare(auxTileDefinition->getTileId())) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void TextureHolder::addTexture(TileDefinition* tileDefinition) {
+	if (duplicateTexture(tileDefinition->getTileId())) {
+		return;
+	}
+
+	this->listOfTextures.push_back(tileDefinition);
+}
+
+SDL_Surface* TextureHolder::getTexture(std::string id) {
+	for (unsigned int i = 0; i < this->listOfTextures.size(); i++) {
+		TileDefinition* auxTileDefinition = this->listOfTextures[i];
+		if (id.compare(auxTileDefinition->getTileId())) {
+			return auxTileDefinition->getTileImage();
+		}
+	}
+
+	return getTexture(DEFAULT_TEXTURE_ID);
+}
+
