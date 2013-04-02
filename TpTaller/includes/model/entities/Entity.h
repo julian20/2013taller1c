e7746a629e32906373ade06e990dba9ee01e6406
gonchu.b <gonchu.b@gmail.com>
2013-04-02 19:44:17 +0000

    Gonchu: Devuelvo la clase entity al proyecto. Ni en pedo la hago de nuevo. Si quieren cambienle el nombre a Personaje, ya que tiene todos los atributos que tendria un personaje, pero el modelo queda mas rico con una clase Speed que con un INTEGER que representa una velocidad.

--- TpTaller/includes/model/entities/Entity.h
+++ TpTaller/includes/model/entities/Entity.h
@@ -9,6 +9,9 @@
 #define ENTITY_H_
 
 #include <model/Vector2.h>
+#include <model/entityProperties/Position.h>
+#include <model/entityProperties/Speed.h>
+#include <model/entityProperties/Power.h>
 
 #include <string>
 #include <vector>
@@ -19,9 +22,23 @@ public:
 	Entity();
 	virtual void setPos(float x, float y);
 	Vector2* GetCurrentPos();
+	Entity(std::string name, Position* position, Speed* speed,
+			std::vector<Power*> powers);
+	const Position* getPosition() const;
+	void setPosition(Position* position);
+	const Speed* getSpeed() const;
+	void setSpeed(Speed* speed);
+	const std::string getName() const;
+	void setName(std::string name);
+	std::vector<Power*> getPowers();
+	void setPowers(std::vector<Power*> powers);
 	virtual ~Entity();
 protected:
 	Vector2* currentPos;
+	Position* position;
+	Speed* speed;
+	std::vector<Power*> powers;
+	std::string name;
 };
 
 #endif /* ENTITY_H_ */
