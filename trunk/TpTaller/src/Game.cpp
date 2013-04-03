4d723cb7383b85bf65292f8694bc43d2c7feb289
gonchu.b@gmail.com <gonchu.b@gmail.com@32cccb14-7060-b6e5-34ce-36fad607224b>
2013-04-03 23:37:42 +0000

    Gonchu: Devolvi los cambios de tomi, pero no anda todavia.

--- trunk/TpTaller/src/Game.cpp
+++ trunk/TpTaller/src/Game.cpp
@@ -6,36 +6,38 @@
 #include <stdio.h>
 
 #include <Game.h>
-#include <view/MapView.h>
 #include <view/entities/PersonajeVista.h>
 #include <model/entities/personaje/Personaje.h>
 #include <model/persistence/PersistentConfiguration.h>
-
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_rotozoom.h>
 #include <SDL/SDL_mixer.h>
 
-#define DEFAULT_W	800		// px
-#define DEFAULT_H	600		// px
-#define DEFAULT_BPP 0		// px
-#define FRAMES_PER_SECOND	30
+
+#define DEFAULT_W       800             // px
+#define DEFAULT_H       600             // px
+#define DEFAULT_BPP 0           // px
+#define FRAMES_PER_SECOND       30
 #define GAME_MUSIC "resources/sound/gameMusic.ogg"
 
 Game::Game(PersistentConfiguration* configuration) {
 
-	MapData* mapData = new MapData(50, 30);
+        MapData* mapData = new MapData(50, 30);
 
-	mapData->SetTileType("soil", 5, 5);
-	mapData->SetTileType("water", 7, 5);
-	mapData->SetTileType("tree", 9, 5);
+        // TODO sacar esto **Tomi**
+        mapData->SetTileType("soil", 5, 5);
+        mapData->SetTileType("water", 7, 5);
 
-	map = new MapView(mapData);
-	map->setTextureHolder(configuration->getTextureHolder());
+        initMusic();
+        initScreen();
 
-	setUpCharacters(map, mapData);
+        mapView = new MapView(mapData,screen);
+        mapView->setTextureHolder(configuration->getTextureHolder());
+        mapController = new MapController(mapView);
 
-	initMusic();
+        setUpCharacters(mapView,mapData);
+
 }
 
 void getEvent() {
@@ -44,18 +46,19 @@ void getEvent() {
 void refreshMap() {
 }
 
-void Game::setUpCharacters(MapView* map, MapData* mapData) {
-	//TODO: Posiblemente se levante del cfg reader esto
-	personaje = new Personaje();
-	personajeVista = new PersonajeVista(personaje, "resources/foo5.png");
-	mapData->addPersonaje(5, 5, personaje);
-	map->AssignPersonaje(personaje);
-	/* Lucas: La linea siguiente antes estaba antes de lo de arriba, pero
-	 * yo creo que va despues, como vas a iterar la matriz de personajes
-	 * seteandolos si no hiciste addpersonaje?
-	 */
-	map->SetUpPersonajes();
+void Game::setUpCharacters(MapView* map,MapData* mapData){
+        //TODO: Posiblemente se levante del cfg reader esto
+        personaje = new Personaje();
+        personajeVista = new PersonajeVista(personaje, "resources/foo5.png");
+        mapData->addPersonaje(5, 5, personaje);
+        map->AssignPersonaje(personaje);
+        /* Lucas: La linea siguiente antes estaba antes de lo de arriba, pero
+         * yo creo que va despues, como vas a iterar la matriz de personajes
+         * seteandolos si no hiciste addpersonaje?
+         */
+        map->SetUpPersonajes();
 }
+
 
 void refreshEntities() {
 }
@@ -64,108 +67,112 @@ void refreshCharacters() {
 }
 
 void Game::initScreen() {
-	SDL_Init(SDL_INIT_EVERYTHING);
 
-	//Buscamos info sobre la resolucion del escritorio y creamos la pantalla
-	const SDL_VideoInfo *info = SDL_GetVideoInfo();
-	if (!info) {
-		pantalla = SDL_SetVideoMode(DEFAULT_W, DEFAULT_H, DEFAULT_BPP,
-				SDL_HWSURFACE);
-	} else {
-		pantalla = SDL_SetVideoMode(info->current_w, info->current_h,
-				info->vfmt->BytesPerPixel / 8, SDL_HWSURFACE);
+        //Buscamos info sobre la resolucion del escritorio y creamos la screen
+        const SDL_VideoInfo *info = SDL_GetVideoInfo();
+                if (!info) {
+                        screen = SDL_SetVideoMode(DEFAULT_W, DEFAULT_H, DEFAULT_BPP,
+                                        SDL_HWSURFACE);
+                } else {
+                        screen = SDL_SetVideoMode(info->current_w, info->current_h,
+                                        info->vfmt->BytesPerPixel / 8, SDL_HWSURFACE);
 
-	}
-	//La hacemos fullscreen
-	/*int flag = 1;
-	 flag = SDL_WM_ToggleFullScreen(pantalla);
-	 if (flag == 0) {
-	 printf("Unable to go fullscreen: %s\n", SDL_GetError());
-	 exit(1);
-	 }
-	 */
+                }
+        //La hacemos fullscreen
+         /*int flag = 1;
+         flag = SDL_WM_ToggleFullScreen(pantalla);
+         if (flag == 0) {
+         printf("Unable to go fullscreen: %s\n", SDL_GetError());
+         exit(1);
+         }
+         */
 
 }
 
 void Game::draw() {
-	//Borramos y redibujamos en cada ciclo
-	SDL_FillRect(pantalla, NULL, 0);
-	map->Draw(pantalla);
+        //Borramos y redibujamos en cada ciclo
 
-	Position* cam = map->GetCamera();
-	personajeVista->UpdateCameraPos(cam->getX(), cam->getY());
-	personajeVista->Mostrar(pantalla);
-	delete cam;
-	// Actualiza la pantalla
-	SDL_Flip(pantalla);
+        mapController->cameraMoveListener();
+
+        SDL_FillRect(screen, NULL, 0);
+        mapView->Draw();
+
+        Position* cam = mapView->GetCamera();
+        personajeVista->UpdateCameraPos(cam->getX(), cam->getY());
+        personajeVista->Mostrar(screen);
+        delete cam;
+        // Actualiza la screen
+        SDL_Flip(screen);
 }
+
 
 MenuEvent Game::run() {
 
-	initScreen();
+        initScreen();
 
-	SDL_Event event;
-	bool exit = false;
-	while (!exit) {
+        SDL_Event event;
+        bool exit = false;
+        while (!exit) {
 
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == 1) {
-				Position* camera = map->GetCamera();
-				map->ClickOn(event.button.x, event.button.y,
-						event.button.button);
-			}
+                while (SDL_PollEvent(&event)) {
+                        if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == 1) {
+                                Position* camera = mapView->GetCamera();
+                                mapView->ClickOn(event.button.x, event.button.y,
+                                                event.button.button);
+                        }
 
-			if (event.type == SDL_QUIT)
-				exit = true;
+                        if (event.type == SDL_QUIT)
+                                return EXIT_EVENT;
 
-			if (event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_ESCAPE)
-				exit = true;
-			//personajeVista->Mostrar();
-		}
+                        if (event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_ESCAPE)
+                                exit = true;
+                        //personajeVista->Mostrar();
+                }
 
-		//Actualizo la parte visual, sin mostrarla todavia
-		//1. Mapa
-		map->Update();
-		// 2. Entidades estaticas
-		//       refreshEntities();
-		// 3. Personaje/s
-		personaje->Update();
+                //Actualizo la parte visual, sin mostrarla todavia
+                //1. Mapa
+                mapView->Update();
+                // 2. Entidades estaticas
+                //       refreshEntities();
+                // 3. Personaje/s
+                personaje->Update();
 
-		// En un futuro, aca se comprueban las colisiones.
-		// y se corrige la posicion del personaje.
+                // En un futuro, aca se comprueban las colisiones.
+                // y se corrige la posicion del personaje.
 
-		// Dibujo
-		draw();
-		SDL_Delay(1000 / FRAMES_PER_SECOND);
-	}
+                // Dibujo
+                draw();
+                SDL_Delay(1000/FRAMES_PER_SECOND);
+        }
 
-	return EXIT_EVENT;
+        return EXIT_EVENT;
+
 }
 
+
 void Game::initMusic() {
-	// Inicializamos la librería SDL_Mixer
-	if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT,
-			MIX_DEFAULT_CHANNELS, 4096) < 0) {
-		cerr << "Subsistema de Audio no disponible" << SDL_GetError() << endl;
-		exit(1);
-	}
-	//startLaugh();
+        // Inicializamos la librería SDL_Mixer
+        if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT,
+                        MIX_DEFAULT_CHANNELS, 4096) < 0) {
+                cerr << "Subsistema de Audio no disponible" << SDL_GetError() << endl;
+                exit(1);
+        }
+        //startLaugh();
 
-	// Cargamos la musica
-	musica = Mix_LoadMUS(GAME_MUSIC);
+        // Cargamos la musica
+        musica = Mix_LoadMUS(GAME_MUSIC);
 
-	if (!musica) {
-		cerr << "No se puede cargar el sonido:" << SDL_GetError() << endl;
-		exit(1);
-	}
-	Mix_VolumeMusic(500);
-	Mix_FadeInMusic(musica, -1, 3000);
+        if (!musica) {
+                cerr << "No se puede cargar el sonido:" << SDL_GetError() << endl;
+                exit(1);
+        }
+        Mix_VolumeMusic(500);
+        Mix_FadeInMusic(musica, -1, 3000);
 
 }
 
 Game::~Game() {
-	Mix_FreeMusic(musica);
-	Mix_CloseAudio();
-	// TODO Auto-generated destructor stub
+        Mix_FreeMusic(musica);
+        Mix_CloseAudio();
+        // TODO Auto-generated destructor stub
 }
-
--- trunk/TpTaller/src/controller/MapController.cpp
+++ trunk/TpTaller/src/controller/MapController.cpp
@@ -8,141 +8,141 @@
 #include <controller/MapController.h>
 #include <iostream>
 
-#define MovementMargin	50	// px
+#define MovementMargin  50      // px
 MapController::MapController(MapView* mapView){
 
-	this->mapView = mapView;
-	this->screen = mapView->getDrawingSurface();
+        this->mapView = mapView;
+        this->screen = mapView->getDrawingSurface();
 }
 
 MouseState MapController::getMouseState() {
-	SDL_GetMouseState(&mouseX, &mouseY);
-	if (mouseAtLeft()) return MOUSE_LEFT;
-	if (mouseAtRight()) return MOUSE_RIGHT;
-	if (mouseAtTop()) return MOUSE_TOP;
-	if (mouseAtBottom()) return MOUSE_BOTTOM;
+        SDL_GetMouseState(&mouseX, &mouseY);
+        if (mouseAtLeft()) return MOUSE_LEFT;
+        if (mouseAtRight()) return MOUSE_RIGHT;
+        if (mouseAtTop()) return MOUSE_TOP;
+        if (mouseAtBottom()) return MOUSE_BOTTOM;
 
-	return MOUSE_CENTER;
+        return MOUSE_CENTER;
 }
 
 bool MapController::mouseAtLeft() {
 
-	if (mouseX < MovementMargin) {
-		if (mouseY > MovementMargin
-				&& mouseY < screen->h - MovementMargin) {
-			return true;
-		}
-	}
+        if (mouseX < MovementMargin) {
+                if (mouseY > MovementMargin
+                                && mouseY < screen->h - MovementMargin) {
+                        return true;
+                }
+        }
 
-	return false;
+        return false;
 }
 
 bool MapController::mouseAtRight() {
 
-	if (mouseX > screen->w - MovementMargin) {
-		if (mouseY > MovementMargin
-				&& mouseY < screen->h - MovementMargin) {
-			return true;
-		}
-	}
+        if (mouseX > screen->w - MovementMargin) {
+                if (mouseY > MovementMargin
+                                && mouseY < screen->h - MovementMargin) {
+                        return true;
+                }
+        }
 
-	return false;
+        return false;
 }
 
 bool MapController::mouseAtTop() {
 
-	if (mouseY < MovementMargin) {
-		if (mouseX > MovementMargin
-				&& mouseX < screen->w - MovementMargin) {
-			return true;
-		}
-	}
+        if (mouseY < MovementMargin) {
+                if (mouseX > MovementMargin
+                                && mouseX < screen->w - MovementMargin) {
+                        return true;
+                }
+        }
 
-	return false;
+        return false;
 }
 
 bool MapController::mouseAtBottom() {
 
-	if (mouseY > screen->h - MovementMargin) {
-		if (mouseX > MovementMargin
-				&& mouseX < screen->w - MovementMargin) {
-			return true;
-		}
-	}
+        if (mouseY > screen->h - MovementMargin) {
+                if (mouseX > MovementMargin
+                                && mouseX < screen->w - MovementMargin) {
+                        return true;
+                }
+        }
 
-	return false;
+        return false;
 }
 
 /* Deprecated (or trying to **TODO** tomi)
 
 bool MapController::MoveScreenLeftUp() {
-	UpdateMouseState();
+        UpdateMouseState();
 
-	if (mouseX < MovementMargin) {
-		if (mouseY < MovementMargin) {
-			return true;
-		}
-	}
+        if (mouseX < MovementMargin) {
+                if (mouseY < MovementMargin) {
+                        return true;
+                }
+        }
 
-	return false;
+        return false;
 }
 
 bool MapController::MoveScreenLeftDown() {
-	UpdateMouseState();
+        UpdateMouseState();
 
-	if (mouseX < MovementMargin) {
-		if (mouseY > desktop->current_h - MovementMargin) {
-			return true;
-		}
-	}
+        if (mouseX < MovementMargin) {
+                if (mouseY > desktop->current_h - MovementMargin) {
+                        return true;
+                }
+        }
 
-	return false;
+        return false;
 }
 
 bool MapController::MoveScreenRightUp() {
-	UpdateMouseState();
+        UpdateMouseState();
 
-	if (mouseX > desktop->current_w - MovementMargin) {
-		if (mouseY < MovementMargin) {
-			return true;
-		}
-	}
+        if (mouseX > desktop->current_w - MovementMargin) {
+                if (mouseY < MovementMargin) {
+                        return true;
+                }
+        }
 
-	return false;
+        return false;
 }
 
 bool MapController::MoveScreenRightDown() {
-	UpdateMouseState();
+        UpdateMouseState();
 
-	if (mouseX > desktop->current_w - MovementMargin) {
-		if (mouseY > desktop->current_h - MovementMargin) {
-			return true;
-		}
-	}
+        if (mouseX > desktop->current_w - MovementMargin) {
+                if (mouseY > desktop->current_h - MovementMargin) {
+                        return true;
+                }
+        }
 
-	return false;
+        return false;
 }
 */
 
 void MapController::cameraMoveListener(){
 
-	MouseState state = getMouseState();
-	switch (state){
-	case MOUSE_TOP:
-		mapView->moveCamera(MOVE_UP);
-		break;
-	case MOUSE_BOTTOM:
-		mapView->moveCamera(MOVE_DOWN);
-		break;
-	case MOUSE_LEFT:
-		mapView->moveCamera(MOVE_LEFT);
-		break;
-	case MOUSE_RIGHT:
-		mapView->moveCamera(MOVE_RIGHT);
-		break;
-	case MOUSE_CENTER:
-		break;
-	}
+        MouseState state = getMouseState();
+        switch (state){
+        case MOUSE_TOP:
+                mapView->moveCamera(MOVE_UP);
+                break;
+        case MOUSE_BOTTOM:
+                mapView->moveCamera(MOVE_DOWN);
+                break;
+        case MOUSE_LEFT:
+                mapView->moveCamera(MOVE_LEFT);
+                break;
+        case MOUSE_RIGHT:
+                mapView->moveCamera(MOVE_RIGHT);
+                break;
+        case MOUSE_CENTER:
+                break;
+        }
 
 }
 
