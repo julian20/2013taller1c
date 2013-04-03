be281832bc4691b47ab7f9ddf011cb97a5c8f860
damian_charango <damian_charango@hotmail.com>
2013-04-03 01:28:06 +0000

    Dami: repair del Logs

--- TpTaller/src/model/Logs/Logs.cpp
+++ TpTaller/src/model/Logs/Logs.cpp
@@ -9,35 +9,37 @@
 
 Logs::Logs(string msj) {
 
-	this->msj = msj;
-	this->archivo = NULL;
+	this->msj=msj;
+	this->archivo=NULL;
 	this->GenerateLogMsj();
-	this->~Logs();
+//	this->~Logs();
 }
 
-void Logs::GenerateLogMsj() {
-	this->archivo = fopen(ruta, "a");
-	if (!this->archivo) {
-		printf(
-				"No se puede continuar con la ejecucion ya que no se pueden manejar los errores\n");
+void Logs::GenerateLogMsj()
+{
+	this->archivo=fopen(ruta,"a");
+	if(!this->archivo)
+	{
+		printf("No se puede continuar con la ejecucion ya que no se pueden manejar los errores\n");
 	}
-	string hora = this->Date();
-	string errorLog = hora + string("-----") + this->msj;
-	fwrite(errorLog.c_str(), 1, errorLog.size(), this->archivo);
-
+	string hora=this->Date();
+	string errorLog= hora+string("--------")+this->msj+string("\n");
+	fwrite(errorLog.c_str(),1,errorLog.size(),this->archivo);
+//	delete[] hora;
+//	delete[] errorLog;
+	fclose(this->archivo);
 }
+
 
 string Logs::Date() {
 
-	time_t tiempo = time(0);
-	struct tm *tlocal = localtime(&tiempo);
-	char salida[128];
-	strftime(salida, 128, "%d/%m/%y %H:%M:%S", tlocal);
-	//printf("%s\n",output);
-
-	return string(salida);
+        time_t tiempo = time(0);
+        struct tm *tlocal = localtime(&tiempo);
+        char salida[128];
+        strftime(salida,128,"%d/%m/%y %H:%M:%S",tlocal);
+        return string(salida);
 }
 Logs::~Logs() {
-	// TODO Auto-generated destructor stub
+	delete this;
 }
 
