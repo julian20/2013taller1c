f330e51d24a213a09b654b215046138127db1f80
lucas.006@live.com.ar <lucas.006@live.com.ar@32cccb14-7060-b6e5-34ce-36fad607224b>
2013-04-09 17:56:29 +0000

    Lucas: agrego el molino.

--- trunk/TpTaller/src/main.cpp
+++ trunk/TpTaller/src/main.cpp
@@ -33,34 +33,34 @@ int main(int argc, char** argv) {
 	ConfigurationReader cfgReader = ConfigurationReader();
 	PersistentConfiguration configuration = cfgReader.loadConfiguration(
 			CONFIGURATION_FILE, OUTPUT_FILENAME);
+	initGame(&configuration);
+//	Menu* menu = new Menu(configuration.getAnimationConfiguration());
 
-	Menu* menu = new Menu(configuration.getAnimationConfiguration());
-
-	MenuEvent event = NOTHING_EVENT;
-	while (event != EXIT_EVENT) {
-
-		event = menu->run();
-		switch (event) {
-		case NOTHING_EVENT:
-			break;
-		case EXIT_EVENT:
-			break;
-		case NEWGAME_EVENT:
-			//Aca inicio el juego
-			menu->close();
-			initGame(&configuration);
-			event = EXIT_EVENT;
-			break;
-		case CONFIG_EVENT:
-			//Aca inicio el menu de configuraciones
-			cout << "Se inicio el menu de configuraciones" << endl;
-			break;
-
-		}
-
-	}
-
-	delete menu;
+//	MenuEvent event = NOTHING_EVENT;
+//	while (event != EXIT_EVENT) {
+//
+//		event = menu->run();
+//		switch (event) {
+//		case NOTHING_EVENT:
+//			break;
+//		case EXIT_EVENT:
+//			break;
+//		case NEWGAME_EVENT:
+//			//Aca inicio el juego
+//			menu->close();
+//			initGame(&configuration);
+//			event = EXIT_EVENT;
+//			break;
+//		case CONFIG_EVENT:
+//			//Aca inicio el menu de configuraciones
+//			cout << "Se inicio el menu de configuraciones" << endl;
+//			break;
+//
+//		}
+//
+//	}
+//
+//	delete menu;
 	return 0;
 }
 
