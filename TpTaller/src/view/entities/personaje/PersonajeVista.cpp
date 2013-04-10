fbf6e5712956e2f421b0890d85eeecc773482325
tomasboccardo <tomasboccardo@gmail.com>
2013-04-10 02:07:56 +0000

    Tomi/Lucas: Agregamos los sprites de running al personaje.

--- TpTaller/src/view/entities/personaje/PersonajeVista.cpp
+++ TpTaller/src/view/entities/personaje/PersonajeVista.cpp
@@ -117,20 +117,44 @@ void PersonajeVista::Mostrar(SDL_Surface* fondo) {
 	Vector2* movementDirection = this->miPersonaje->GetMovementDirection();
 	float direction = movementDirection->GetAngle();
 
-	if (M_PI * 15 / 8 < direction || direction < M_PI * 1 / 8)
+	if ((M_PI * 15 / 8 < direction || direction < M_PI * 1 / 8)
+			&& miPersonaje->isRunning())
+		clipToDraw = &clipsDerechaRun[marco];
+	else if (M_PI * 15 / 8 < direction || direction < M_PI * 1 / 8)
 		clipToDraw = &clipsDerecha[marco];
+	else if ((M_PI * 1 / 8 < direction && direction < M_PI * 3 / 8)
+			&& miPersonaje->isRunning())
+		clipToDraw = &clipsAbajoDerRun[marco];
 	else if (M_PI * 1 / 8 < direction && direction < M_PI * 3 / 8)
 		clipToDraw = &clipsAbajoDer[marco];
+	else if ((M_PI * 3 / 8 < direction && direction < M_PI * 5 / 8)
+			&& miPersonaje->isRunning())
+		clipToDraw = &clipsAbajoRun[marco];
 	else if (M_PI * 3 / 8 < direction && direction < M_PI * 5 / 8)
 		clipToDraw = &clipsAbajo[marco];
+	else if ((M_PI * 5 / 8 < direction && direction < M_PI * 7 / 8)
+			&& miPersonaje->isRunning())
+		clipToDraw = &clipsAbajoIzqRun[marco];
 	else if (M_PI * 5 / 8 < direction && direction < M_PI * 7 / 8)
 		clipToDraw = &clipsAbajoIzq[marco];
+	else if ((M_PI * 7 / 8 < direction && direction < M_PI * 9 / 8)
+			&& miPersonaje->isRunning())
+		clipToDraw = &clipsIzquierdaRun[marco];
 	else if (M_PI * 7 / 8 < direction && direction < M_PI * 9 / 8)
 		clipToDraw = &clipsIzquierda[marco];
+	else if ((M_PI * 9 / 8 < direction && direction < M_PI * 11 / 8)
+			&& miPersonaje->isRunning())
+		clipToDraw = &clipsArribaIzqRun[marco];
 	else if (M_PI * 9 / 8 < direction && direction < M_PI * 11 / 8)
 		clipToDraw = &clipsArribaIzq[marco];
+	else if ((M_PI * 11 / 8 < direction && direction < M_PI * 13 / 8)
+			&& miPersonaje->isRunning())
+		clipToDraw = &clipsArribaRun[marco];
 	else if (M_PI * 11 / 8 < direction && direction < M_PI * 13 / 8)
 		clipToDraw = &clipsArriba[marco];
+	else if ((M_PI * 13 / 8 < direction && direction < M_PI * 15 / 8)
+			&& miPersonaje->isRunning())
+		clipToDraw = &clipsArribaDerRun[marco];
 	else if (M_PI * 13 / 8 < direction && direction < M_PI * 15 / 8)
 		clipToDraw = &clipsArribaDer[marco];
 
@@ -158,61 +182,124 @@ void PersonajeVista::EstablecerLosClips() {
 
 	for (int i = 0; i < numberOfClips; i++) {
 		SDL_Rect rect;
-
+		int j = 0;
 		clipsIzquierda.push_back(rect);
 		clipsIzquierda[i].x = imageWidth * i;
-		clipsIzquierda[i].y = imageHeight * 0;
+		clipsIzquierda[i].y = imageHeight * j;
 		clipsIzquierda[i].w = imageWidth;
 		clipsIzquierda[i].h = imageHeight;
+		j++;
 
 		clipsAbajoIzq.push_back(rect);
 		clipsAbajoIzq[i].x = imageWidth * i;
-		clipsAbajoIzq[i].y = imageHeight * 1;
+		clipsAbajoIzq[i].y = imageHeight * j;
 		clipsAbajoIzq[i].w = imageWidth;
 		clipsAbajoIzq[i].h = imageHeight;
+		j++;
 
 		clipsAbajoDer.push_back(rect);
 		clipsAbajoDer[i].x = imageWidth * i;
-		clipsAbajoDer[i].y = imageHeight * 2;
+		clipsAbajoDer[i].y = imageHeight * j;
 		clipsAbajoDer[i].w = imageWidth;
 		clipsAbajoDer[i].h = imageHeight;
+		j++;
 
 		clipsAbajo.push_back(rect);
 		clipsAbajo[i].x = imageWidth * i;
-		clipsAbajo[i].y = imageHeight * 3;
+		clipsAbajo[i].y = imageHeight * j;
 		clipsAbajo[i].w = imageWidth;
 		clipsAbajo[i].h = imageHeight;
+		j++;
 
 		clipsArribaIzq.push_back(rect);
 		clipsArribaIzq[i].x = imageWidth * i;
-		clipsArribaIzq[i].y = imageHeight * 4;
+		clipsArribaIzq[i].y = imageHeight * j;
 		clipsArribaIzq[i].w = imageWidth;
 		clipsArribaIzq[i].h = imageHeight;
+		j++;
 
 		clipsArribaDer.push_back(rect);
 		clipsArribaDer[i].x = imageWidth * i;
-		clipsArribaDer[i].y = imageHeight * 5;
+		clipsArribaDer[i].y = imageHeight * j;
 		clipsArribaDer[i].w = imageWidth;
 		clipsArribaDer[i].h = imageHeight;
+		j++;
 
 		clipsArriba.push_back(rect);
 		clipsArriba[i].x = imageWidth * i;
-		clipsArriba[i].y = imageHeight * 6;
+		clipsArriba[i].y = imageHeight * j;
 		clipsArriba[i].w = imageWidth;
 		clipsArriba[i].h = imageHeight;
+		j++;
 
 		clipsDerecha.push_back(rect);
 		clipsDerecha[i].x = imageWidth * i;
-		clipsDerecha[i].y = imageHeight * 7;
+		clipsDerecha[i].y = imageHeight * j;
 		clipsDerecha[i].w = imageWidth;
 		clipsDerecha[i].h = imageHeight;
+		j++;
+
+		clipsIzquierdaRun.push_back(rect);
+		clipsIzquierdaRun[i].x = imageWidth * i;
+		clipsIzquierdaRun[i].y = imageHeight * j;
+		clipsIzquierdaRun[i].w = imageWidth;
+		clipsIzquierdaRun[i].h = imageHeight;
+		j++;
+
+		clipsAbajoIzqRun.push_back(rect);
+		clipsAbajoIzqRun[i].x = imageWidth * i;
+		clipsAbajoIzqRun[i].y = imageHeight * j;
+		clipsAbajoIzqRun[i].w = imageWidth;
+		clipsAbajoIzqRun[i].h = imageHeight;
+		j++;
+
+		clipsAbajoDerRun.push_back(rect);
+		clipsAbajoDerRun[i].x = imageWidth * i;
+		clipsAbajoDerRun[i].y = imageHeight * j;
+		clipsAbajoDerRun[i].w = imageWidth;
+		clipsAbajoDerRun[i].h = imageHeight;
+		j++;
+
+		clipsAbajoRun.push_back(rect);
+		clipsAbajoRun[i].x = imageWidth * i;
+		clipsAbajoRun[i].y = imageHeight * j;
+		clipsAbajoRun[i].w = imageWidth;
+		clipsAbajoRun[i].h = imageHeight;
+		j++;
+
+		clipsArribaIzqRun.push_back(rect);
+		clipsArribaIzqRun[i].x = imageWidth * i;
+		clipsArribaIzqRun[i].y = imageHeight * j;
+		clipsArribaIzqRun[i].w = imageWidth;
+		clipsArribaIzqRun[i].h = imageHeight;
+		j++;
+
+		clipsArribaDerRun.push_back(rect);
+		clipsArribaDerRun[i].x = imageWidth * i;
+		clipsArribaDerRun[i].y = imageHeight * j;
+		clipsArribaDerRun[i].w = imageWidth;
+		clipsArribaDerRun[i].h = imageHeight;
+		j++;
+
+		clipsArribaRun.push_back(rect);
+		clipsArribaRun[i].x = imageWidth * i;
+		clipsArribaRun[i].y = imageHeight * j;
+		clipsArribaRun[i].w = imageWidth;
+		clipsArribaRun[i].h = imageHeight;
+		j++;
+
+		clipsDerechaRun.push_back(rect);
+		clipsDerechaRun[i].x = imageWidth * i;
+		clipsDerechaRun[i].y = imageHeight * j;
+		clipsDerechaRun[i].w = imageWidth;
+		clipsDerechaRun[i].h = imageHeight;
+		j++;
 
 		clipsQuieto.push_back(rect);
 		clipsQuieto[i].x = imageWidth * i;
-		clipsQuieto[i].y = imageHeight * 8;
+		clipsQuieto[i].y = imageHeight * j;
 		clipsQuieto[i].w = imageWidth;
 		clipsQuieto[i].h = imageHeight;
-
 	}
 }
 
