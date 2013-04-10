98df3741561343a856cba3e724b64e7dc491b36f
damian_charango@hotmail.com <damian_charango@hotmail.com@32cccb14-7060-b6e5-34ce-36fad607224b>
2013-04-10 18:57:44 +0000

    Dami: Subo lo del zoom. pero escalado en 1. osea lo van a ver igual. En caso de q hoy me den el ok. Ya lo tenemos hecho

--- trunk/TpTaller/src/model/entities/Entity.cpp
+++ trunk/TpTaller/src/model/entities/Entity.cpp
@@ -6,7 +6,6 @@
  */
 
 #include <model/entities/Entity.h>
-#include <model/entities/Entity.h>
 #include <model/map/Tile.h>
 
 #include <vector>
@@ -16,52 +15,51 @@
 using namespace std;
 
 Entity::Entity() {
-	this->currentPos = new Vector2(0, 0);
-	this->coord = new Coordinates(0,0);
-	this->base = new Base();
-	this->name = "";
+        this->currentPos = new Vector2(0, 0);
+        this->coord = new Coordinates(0,0);
+        this->base = new Base();
+        this->name = "";
 }
 
 void Entity::setPos(float x, float y) {
-	currentPos->SetValues(x,y);
-	coord->changeTo((int) x/Tile::computePosition(0,0).w , (int) y/Tile::computePosition(0,0).h);
+        currentPos->SetValues(x,y);
+        coord->changeTo((int) x/Tile::computePosition(0,0).w , (int) y/Tile::computePosition(0,0).h);
 }
 
 Vector2* Entity::getCurrentPos() {
-	return currentPos;
+        return currentPos;
 }
 
 Coordinates* Entity::getCoordinates() {
-	return coord;
+        return coord;
 }
 
 void Entity::setCoordinates(int x, int y){
-	coord->changeTo(x,y);
+        coord->changeTo(x,y);
 }
 
 string Entity::getClassName() {
-	return "Entity";
+        return "Entity";
 }
 
 Base* Entity::getBase() {
-	return this->base;
+        return this->base;
 }
 
 void Entity::setBase(Base* base) {
-	this->base = base;
+        this->base = base;
 }
 
 std::string Entity::getName() {
-	return this->name;
+        return this->name;
 }
 
 void Entity::setName(std::string name) {
-	this->name = name;
+        this->name = name;
 }
 
 Entity::~Entity() {
-	delete this->coord;
-	delete this->currentPos;
-	delete this->base;
+        delete this->coord;
+        delete this->currentPos;
+        delete this->base;
 }
-
--- trunk/TpTaller/src/view/entities/EntityView.cpp
+++ trunk/TpTaller/src/view/entities/EntityView.cpp
@@ -5,25 +5,33 @@
  * Created on 23 de marzo de 2013, 12:25
  */
 
-#include <view/entities/PlayerView.h>
+//#include <view/entities/PersonajeVista.h>
+#include <view/entities/EntityView.h>
 #include <stdio.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_rotozoom.h>
 #include <cmath>
 #include <string>
+#include <SDL/SDL_rotozoom.h>
 
-#define DELAY 2 //seconds
 
 using namespace std;
+#define DELAY 2
 
 EntityView::EntityView() {
+	this->baseHeight=1;
+	this->baseWidth=1;
+	this->scaleWidth=1;
+	this->scaleHeight=1;
 	this->imagePath = "";
-	this->imageHeight = 0;
-	this->imageWidth = 0;
+	this->tileHeight=50;
+	this->tileWidth=70;
+	this->imageHeight=0;
+	this->imageWidth=0;
 	this->entity = NULL;
 	this->image = NULL;
 	this->nClips = 1;
-	this->currentClip = 0;
+	this->currentClip=0;
 	this->anchorPixel = new Vector2(0, 0);
 	this->movable = false;
 	timer.start();
@@ -34,50 +42,86 @@ EntityView::EntityView() {
 bool EntityView::isMovable(){
 	return movable;
 }
+void EntityView::setScale()
+{
+	//baseHeight = entity->getBase()->getLength();
+	//baseWidth = entity->getBase()->getWidth();
+	this->scaleHeight=(float)(this->tileHeight)*float(this->baseHeight)/(float)(this->imageHeight);
+	this->scaleWidth=(float)(this->tileWidth)*(float)(this->baseWidth)/(float)(this->imageWidth);
+	printf("ancho %f---- alto %f",scaleWidth,scaleHeight);
+}
 
-void EntityView::setImageWidth(int width) {
+void EntityView::setImageWidth(int width)
+{
 	this->imageWidth = width;
 }
 
-
-void EntityView::setNClips(int clips) {
-	this->nClips = clips;
+void EntityView::setNClips(int clips)
+{
+	this->nClips=clips;
 }
 
-void EntityView::setImageHeight(int height) {
+void EntityView::setTileWidth(int width)
+{
+	this->tileWidth=width;
+}
+void EntityView::setTileHeight(int height)
+{
+	this->tileHeight=height;
+}
+void EntityView::setBaseWidth(int width)
+{
+	this->baseWidth=width;
+}
+void EntityView::setBaseHeight(int height)
+{
+	this->baseHeight=height;
+}
+
+void EntityView::setImageHeight(int height)
+{
 	this->imageHeight = height;
 }
 
 SDL_Surface* EntityView::load_image(string urlImagen) {
+
 	//The image that's loaded
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Surface* loadedImage = NULL;
 
 	//The optimized surface that will be used
 	SDL_Surface* optimizedImage = NULL;
-
+	//this->setScale();
 	loadedImage = IMG_Load(urlImagen.c_str());
-
+	loadedImage = rotozoomSurfaceXY( loadedImage, 0, scaleWidth, scaleHeight, 0 );
 	if (loadedImage != NULL) {
 		//loadedImage = rotozoomSurfaceXY(loadedImage, 0, 2, 2, 0);
 
 		//Create an optimized surface
 		optimizedImage = SDL_DisplayFormatAlpha(loadedImage);
 
+
+
 		if (optimizedImage != NULL) {
 			//Color key surface
 			SDL_FreeSurface(loadedImage);
 			SDL_SetColorKey(optimizedImage, SDL_SRCCOLORKEY,
 					SDL_MapRGB(optimizedImage->format, 0, 0xFF, 0xFF));
-		} else
+		}
+		else
 			optimizedImage = loadedImage;
 	}
 
 	return optimizedImage;
 }
+/*void EntityView::Draw()
+{
+	SDL_BlitSurface(source, clip, screen, &offset);
+}*/
 
 void EntityView::setImagePath(string image_path) {
 	this->imagePath = image_path;
+	cout << image_path << endl;
 	this->image = load_image(image_path);
 	if (!image) { //TODO al log / loadear alternativa
 		cout << "Error al cargar imagen de la vista " << image_path << endl;
@@ -105,36 +149,35 @@ Vector2* EntityView::getAnchorPixel() {
 	return this->anchorPixel;
 }
 /*
- void EntityView::EstablecerClips()
- {
- for(int x=0; x < this->nClips ; x++)
- {
- clip[x].x =this->imageWidth*x;
- clip[x].y =0;
- clip[x].w=this->imageWidth;
- clip[x].h=this->imageHeight;
+void EntityView::EstablecerClips()
+{
+	for(int x=0; x < this->nClips ; x++)
+	{
+		clip[x].x =this->imageWidth*x;
+		clip[x].y =0;
+		clip[x].w=this->imageWidth;
+		clip[x].h=this->imageHeight;
 
- }
- }*/
+	}
+}*/
 
 void EntityView::draw(SDL_Surface* screen, Position* cam) {
 
-	clip.x = this->imageWidth * this->currentClip;
-	clip.y = 0;
-	clip.w = this->imageWidth;
-	clip.h = this->imageHeight;
+	clip.x =this->imageWidth*this->currentClip*scaleWidth;
+	clip.y =0;
+	clip.w=this->imageWidth*scaleWidth;
+	clip.h=this->imageHeight*scaleHeight;
 	Vector2* position = entity->getCurrentPos();
 	int x = (int) position->GetX();
 	int y = (int) position->GetY();
 
 	SDL_Rect offset;
-	offset.x = (int) (Tile::computePosition(x, y).x + cam->getX() - clip.w / 2);
-	offset.y = (int) (Tile::computePosition(x, y).y + cam->getY() - clip.h / 2);
+	offset.x = (int) (Tile::computePosition(x,y).x + cam->getX() - clip.w/2);
+	offset.y = (int) (Tile::computePosition(x,y).y + cam->getY() - clip.h/2);
 	offset.h = clip.h;
 	offset.w = clip.w;
 
 	SDL_BlitSurface(image, &clip, screen, &offset);
-
 	timeSinceLastAnimation = timer.getTimeSinceLastAnimation();
 	if (currentClip < this->nClips && timeSinceLastAnimation >= DELAY*1000) {
 		currentClip++;
