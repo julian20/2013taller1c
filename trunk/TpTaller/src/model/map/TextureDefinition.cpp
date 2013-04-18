b7ae2af040cdfcc0795c324e5774cd141f0638d8
federico1245@hotmail.com <federico1245@hotmail.com@32cccb14-7060-b6e5-34ce-36fad607224b>
2013-04-18 14:51:04 +0000

    Fede: La puta madre me olvide de subir los archivos.

--- trunk/TpTaller/src/model/map/TextureDefinition.cpp
+++ trunk/TpTaller/src/model/map/TextureDefinition.cpp
@@ -5,7 +5,7 @@
  *      Author: gonchub
  */
 
-#include <model/map/TileDefinition.h>
+#include <model/map/TextureDefinition.h>
 #include <SDL/SDL.h>
 #include <string>
 #include <SDL/SDL_image.h>
@@ -15,44 +15,44 @@
 
 #define TilesScale  	1
 
-#define DEFAULT_TEXTURE_PATH "resources/texturesTiles/grass.png"
+#define DEFAULT_TEXTURE_PATH "resources/questionMark.png"
 
-TileDefinition::TileDefinition() {
+TextureDefinition::TextureDefinition() {
 	this->id = "default";
-	this->setDefaultTileImage(DEFAULT_TEXTURE_PATH);
+	this->setDefaultImage(DEFAULT_TEXTURE_PATH);
 	this->imageSrc = this->defaultImgSrc;
 	this->openImage = NULL;
 }
 
-TileDefinition::TileDefinition(std::string id, std::string imageSrc) {
+TextureDefinition::TextureDefinition(std::string id, std::string imageSrc) {
 	this->id = id;
 	this->imageSrc = imageSrc;
-	this->setDefaultTileImage(DEFAULT_TEXTURE_PATH);
+	this->setDefaultImage(DEFAULT_TEXTURE_PATH);
 	this->openImage = NULL;
 }
 
-TileDefinition::~TileDefinition() {
+TextureDefinition::~TextureDefinition() {
 	SDL_FreeSurface(openImage);
 }
 
-std::string TileDefinition::getTileId() {
+std::string TextureDefinition::getTextureId() {
 	return this->id;
 }
 
-void TileDefinition::setTileId(std::string id) {
+void TextureDefinition::setTextureId(std::string id) {
 	this->id = id;
 }
 
-std::string TileDefinition::getTileImageSrc() {
+std::string TextureDefinition::getTextureImageSrc() {
 	return this->imageSrc;
 }
 
-SDL_Surface* TileDefinition::getTileImage() {
+SDL_Surface* TextureDefinition::getTextureImage() {
 	if (this->openImage == NULL) {
 		SDL_Surface* loadedImageTmp = IMG_Load(this->imageSrc.c_str());
 		if (loadedImageTmp == NULL) {
 			Logs::logErrorMessage("Unable to load tile texture: " + string(SDL_GetError()));
-			loadedImageTmp = this->getDefaultTileImage();
+			loadedImageTmp = this->getDefaultTextureImage();
 		}
 
 		SDL_Surface* loadedImageRot = rotozoomSurfaceXY(loadedImageTmp, 0, TilesScale,
@@ -65,11 +65,11 @@ SDL_Surface* TileDefinition::getTileImage() {
 	return this->openImage;
 }
 
-void TileDefinition::setTileImageSrc(std::string imageSrc) {
+void TextureDefinition::setTextureImageSrc(std::string imageSrc) {
 	this->imageSrc = imageSrc;
 }
 
-SDL_Surface* TileDefinition::prepareImage(SDL_Surface* loadedImage) {
+SDL_Surface* TextureDefinition::prepareImage(SDL_Surface* loadedImage) {
 	SDL_Surface* tmp = loadedImage;
 	loadedImage = rotozoomSurfaceXY(tmp, 0, TilesScale, TilesScale, 0);
 	SDL_FreeSurface(tmp);
@@ -79,7 +79,7 @@ SDL_Surface* TileDefinition::prepareImage(SDL_Surface* loadedImage) {
 	return loadedImage;
 }
 
-SDL_Surface * TileDefinition::getDefaultTileImage() {
+SDL_Surface * TextureDefinition::getDefaultTextureImage() {
 	SDL_Surface* loadedImage = IMG_Load(this->defaultImgSrc.c_str());
 	if (loadedImage == NULL) {
 		Logs::logErrorMessage("Unable to load default tile texture: " + string(SDL_GetError()));
@@ -89,6 +89,6 @@ SDL_Surface * TileDefinition::getDefaultTileImage() {
 	return loadedImage;
 }
 
-void TileDefinition::setDefaultTileImage(std::string imageSrc) {
+void TextureDefinition::setDefaultImage(std::string imageSrc) {
 	this->defaultImgSrc = imageSrc;
 }
