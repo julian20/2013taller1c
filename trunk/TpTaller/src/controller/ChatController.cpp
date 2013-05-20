1665a09fd2ecdd978891aad0ec83c83d18b01d1e
damian_charango@hotmail.com <damian_charango@hotmail.com@32cccb14-7060-b6e5-34ce-36fad607224b>
2013-05-20 01:16:01 +0000

    Dami: fix parcial del server

--- trunk/TpTaller/src/controller/ChatController.cpp
+++ trunk/TpTaller/src/controller/ChatController.cpp
@@ -25,119 +25,124 @@ void ChatController::resetString()
 }
 void ChatController::handle_events(SDL_Event ev) {
 
-	if ((ev.type == SDL_MOUSEBUTTONDOWN)
-			&& (ev.button.button == SDL_BUTTON_RIGHT)) {
-			int x = ev.button.x;
-			int y = ev.button.y;
-			if(this->playerController->clickAnotherPlayer(x,y))
-				{
-					this->chat->Enable();
-					this->chat->setReceptor(this->playerController->getLastPlayerTouch());
-					this->receptor = this->playerController->getLastPlayerTouch();
-					this->chat->setReceptor(this->receptor);
-				}
-	} else if (ev.type == SDL_KEYDOWN ) {
-		if (ev.key.keysym.sym == SDLK_KP_ENTER || ev.key.keysym.sym == SDLK_RETURN)
-				{
-					if (this->text != ""){
-						string playerName = this->playerController->getPlayer()->getName();
-						ChatMessage* cm = new ChatMessage();
-						replace(this->text.begin(), this->text.end(), ' ', '/');
-						cm->setMSJ(this->text);
-						cm->setReceptor(receptor);
-						cm->setSender(playerName);
-						this->chat->newMessageSend(cm);
-						this->text="";
-					}
 
-				}
-		else{
-		char c = (char) ev.key.keysym.sym;
-		if (c >= 'a' && c <= 'z') {
-			SDLMod m = ev.key.keysym.mod;
-			c = (m & KMOD_CAPS) || (m & KMOD_SHIFT) ?
-					toupper((char) ev.key.keysym.sym) :
-					(char) ev.key.keysym.sym;
-			this->text += c;
-		} else if ((c <= '1' || c >= '0')
-				&& ev.key.keysym.sym != SDLK_LSHIFT && ev.key.keysym.sym !=SDLK_RSHIFT) {
-			if (ev.key.keysym.mod & KMOD_SHIFT) {
-				switch (c) {
-				case '1':
-					c = '!';
-					break;
-				case '2':
-					c = '"';
-					break;
-				case '3':
-					c = '#';
-					break;
-				case '4':
-					c = '$';
-					break;
-				case '5':
-					c = '%';
-					break;
-				case '6':
-					c = '^';
-					break;
-				case '7':
-					c = '&';
-					break;
-				case '8':
-					c = '*';
-					break;
-				case '9':
-					c = '(';
-					break;
-				case '0':
-					c = ')';
-					break;
-				case '-':
-					c = '_';
-					break;
-				case '=':
-					c = '+';
-					break;
-				case '[':
-					c = '{';
-					break;
-				case ']':
-					c = '}';
-					break;
-				case ';':
-					c = ':';
-					break;
-				case '\'':
-					c = '@';
-					break;
-				case '#':
-					c = '~';
-					break;
-				case ',':
-					c = '<';
-					break;
-				case '.':
-					c = '>';
-					break;
-				case '/':
-					c = '?';
-					break;
-				default:
-					break;
-				}
-			}
-			this->text += c;
-			//Si apreta borrar armo un substring con dos letras menos
+		if ((ev.type == SDL_MOUSEBUTTONDOWN)
+				&& (ev.button.button == SDL_BUTTON_RIGHT)) {
+				int x = ev.button.x;
+				int y = ev.button.y;
+				if(this->playerController->clickAnotherPlayer(x,y))
+					{
+						this->chat->Enable();
+						this->chat->setReceptor(this->playerController->getLastPlayerTouch());
+						this->receptor = this->playerController->getLastPlayerTouch();
+						this->chat->setReceptor(this->receptor);
+					}
 		}
-		if (ev.key.keysym.sym == SDLK_BACKSPACE) {
-			int longitud = this->text.length() - 2;
-			string aux = (this->text).substr(0, longitud);
-			this->text = aux;
+		if(this->chat->isEnable())
+		{
+			 if (ev.type == SDL_KEYDOWN ) {
+			if (ev.key.keysym.sym == SDLK_KP_ENTER || ev.key.keysym.sym == SDLK_RETURN)
+					{
+						if (this->text != ""){
+							string playerName = this->playerController->getPlayer()->getName();
+							ChatMessage* cm= new ChatMessage();
+							replace(this->text.begin(), this->text.end(), ' ', '/');
+							cm->setMSJ(this->text);
+							cm->setReceptor(receptor);
+							cm->setSender(playerName);
+							this->chat->newMessageSend(cm);
+							this->text="";
+						}
+
+					}
+			else{
+			char c = (char) ev.key.keysym.sym;
+			if (c >= 'a' && c <= 'z') {
+				SDLMod m = ev.key.keysym.mod;
+				c = (m & KMOD_CAPS) || (m & KMOD_SHIFT) ?
+						toupper((char) ev.key.keysym.sym) :
+						(char) ev.key.keysym.sym;
+				this->text += c;
+			} else if ((c <= '1' || c >= '0')
+					&& ev.key.keysym.sym != SDLK_LSHIFT && ev.key.keysym.sym !=SDLK_RSHIFT) {
+				if (ev.key.keysym.mod & KMOD_SHIFT) {
+					switch (c) {
+					case '1':
+						c = '!';
+						break;
+					case '2':
+						c = '?';
+						break;
+					case '3':
+						c = '#';
+						break;
+					case '4':
+						c = '$';
+						break;
+					case '5':
+						c = '%';
+						break;
+					case '6':
+						c = '^';
+						break;
+					case '7':
+						c = '&';
+						break;
+					case '8':
+						c = '*';
+						break;
+					case '9':
+						c = '(';
+						break;
+					case '0':
+						c = ')';
+						break;
+					case '-':
+						c = '_';
+						break;
+					case '=':
+						c = '+';
+						break;
+					case '[':
+						c = '{';
+						break;
+					case ']':
+						c = '}';
+						break;
+					case ';':
+						c = ':';
+						break;
+					case '\'':
+						c = '@';
+						break;
+					case '#':
+						c = '~';
+						break;
+					case ',':
+						c = '<';
+						break;
+					case '/':
+						c = '?';
+						break;
+					case '.':
+						this->chat->Disable();
+						break;
+					default:
+						break;
+					}
+				}
+				this->text += c;
+				//Si apreta borrar armo un substring con dos letras menos
+			}
+			if (ev.key.keysym.sym == SDLK_BACKSPACE) {
+				int longitud = this->text.length() - 2;
+				string aux = (this->text).substr(0, longitud);
+				this->text = aux;
+			}
 		}
 	}
-}
-	this->chat->setWrittingMsj(this->text);
+		this->chat->setWrittingMsj(this->text);
+	}
 }
 string ChatController::getText()
 {
