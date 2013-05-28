1f606d532d004d9ce6b0e581a801a2868236e009
tomasboccardo <tomasboccardo@gmail.com>
2013-05-28 17:20:51 +0000

    Tomi: Redirigo sterr en el script que arme, asi no tira errores

--- TpTaller/copyServerSendFile.sh
+++ TpTaller/copyServerSendFile.sh
@@ -1,7 +1,11 @@
 
+if [ $# -ne 2 ]; then
+	exit
+fi
+
 s_file=$1
 d_file=$2
 
-num=`grep -n playerLocations $s_file | cut -d ':' -f 1`
-num=`echo $num+1 | bc`
-sed "${num}s/^.*$/  - name: NAME_TAG/g" $s_file > $d_file
+num=`grep -n playerLocations $s_file 2>/dev/null | cut -d ':' -f 1  >/dev/null 2>&1`
+num=`echo $num+1 | bc   > /dev/null 2>&1`
+sed "${num}s/^.*$/  - name: NAME_TAG/g" $s_file 2>/dev/null 1> $d_file 
