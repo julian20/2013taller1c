1099fe3c40522c66aef770fa03474f30bf7acb13
gonchu.b@gmail.com <gonchu.b@gmail.com@32cccb14-7060-b6e5-34ce-36fad607224b>
2013-06-09 17:50:40 +0000

    Gonchu: SpellShieldItem para lucas.

---
+++ trunk/TpTaller/src/model/entities/Items/SpellShieldItem.cpp
@@ -0,0 +1,42 @@
+/*
+ * SpellShieldItem.cpp
+ *
+ *  Created on: Jun 9, 2013
+ *      Author: gonchub
+ */
+
+#include <model/entities/Items/SpellShieldItem.h>
+
+SpellShieldItem::SpellShieldItem() {
+}
+
+SpellShieldItem::SpellShieldItem(Item* entity) : Item(entity) {
+	Vector3* pos = entity->getCurrentPos();
+	this->currentPos = new Vector3(pos->getX(), pos->getY(), pos->getZ());
+	Coordinates coordin = entity->getCoordinates();
+	this->coord = new Coordinates(coordin.getCol(), coordin.getRow());
+	Base base = entity->getBase();
+	Base* newBase = new Base(base);
+	this->base = newBase;
+	this->name = entity->getName();
+	this->life = entity->getLife();
+	this->currentTile = NULL;
+	this->team = entity->getTeam();
+	this->killedBy = entity->getKilledBy();
+}
+
+void SpellShieldItem::applyEffects(Player& entity) {
+	// TODO: lucas agregar spell.
+}
+
+void SpellShieldItem::collideTo(Player& entity) {
+	applyEffects(entity);
+}
+
+string SpellShieldItem::getClassName() {
+	return "SpellShieldItem";
+}
+
+SpellShieldItem::~SpellShieldItem() {
+}
+
