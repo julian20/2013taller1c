9e35c18d7f9632f43d41c21ef056033094c85b78
gonchu.b <gonchu.b@gmail.com>
2013-06-09 22:47:22 +0000

    Gonchu: le bajo la vida a la flag y meto un if por las dudas.

--- TpTaller/src/model/entities/Flag.cpp
+++ TpTaller/src/model/entities/Flag.cpp
@@ -8,28 +8,32 @@
 #include <model/entities/Flag.h>
 
 Flag::Flag() {
-	player="";
-	name="Flag";
-}
-void Flag::attackedBy(string player)
-{
-	this->player=player;
-}
-string Flag::getClassName()
-{
-	return this->name;
-}
-void Flag::collideTo(MobileEntity& player) {
-	        receiveDamage(player);
-	        if ( life <= 0 ) {
-	               killedBy = player.getTeam();
-	      }
+	player = "";
+	name = "Flag";
+	life = 30;
 }
 
-void Flag::receiveDamage(MobileEntity& player)
-{
-	life-=player.getDamageBuffer();
+void Flag::attackedBy(string player) {
+	this->player = player;
 }
+
+string Flag::getClassName() {
+	return this->name;
+}
+
+void Flag::collideTo(MobileEntity& player) {
+	if (!isDead()) {
+		receiveDamage(player);
+		if (life <= 0) {
+			killedBy = player.getTeam();
+		}
+	}
+}
+
+void Flag::receiveDamage(MobileEntity& player) {
+	life -= player.getDamageBuffer();
+}
+
 Flag::~Flag() {
 }
 
