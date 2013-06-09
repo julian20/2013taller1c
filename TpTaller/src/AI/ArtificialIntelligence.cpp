2332ba77c657f54f6ec4dc19b24b714410d7c185
gonchu.b <gonchu.b@gmail.com>
2013-06-09 18:37:21 +0000

    Gonchu: las mobs se quedan quietas cuando estan muertas.

--- TpTaller/src/AI/ArtificialIntelligence.cpp
+++ TpTaller/src/AI/ArtificialIntelligence.cpp
@@ -19,7 +19,7 @@ void ArtificialIntelligence::setMobileEntity(MobileEntity* entity) {
 
 void ArtificialIntelligence::update(MapData* mapData) {
 
-	if (entity->getAttackToEntity() != NULL) {
+	if (entity->getAttackToEntity() != NULL && !entity->isDead()) {
 		Tile* currentTile = entity->getTile();
 		Tile* enemyTile = entity->getAttackToEntity()->getTile();
 
@@ -27,12 +27,11 @@ void ArtificialIntelligence::update(MapData* mapData) {
 			return;
 	}
 
-
-	if (this->isAnyEnemyClose(mapData)) {
-			Entity& enemy = this->getNearestEnemy();
-				entity->attackTo(&enemy);
+	if (this->isAnyEnemyClose(mapData) && !entity->isDead()) {
+		Entity& enemy = this->getNearestEnemy();
+		entity->attackTo(&enemy);
 	} else {
-		if(entity->getClassName() == "MobileEntity") {
+		if (entity->getClassName() == "MobileEntity") {
 			entity->cancelAttack();
 			this->watch(mapData);
 		}
@@ -42,12 +41,13 @@ void ArtificialIntelligence::update(MapData* mapData) {
 
 bool ArtificialIntelligence::isAnyEnemyClose(MapData* mapData) {
 
-	entitiesNear = mapData->getClosestEntities(entity->getCoordinates(),WATCHSIZE);
+	entitiesNear = mapData->getClosestEntities(entity->getCoordinates(),
+			WATCHSIZE);
 	list<MobileEntity*>::iterator iter;
 
-	for ( iter = entitiesNear.begin() ; iter != entitiesNear.end() ; ++iter ){
+	for (iter = entitiesNear.begin(); iter != entitiesNear.end(); ++iter) {
 		MobileEntity* mob = *iter;
-		if(mob->getTeam() != this->entity->getTeam())
+		if (mob->getTeam() != this->entity->getTeam())
 			return true;
 	}
 	return false;
@@ -58,53 +58,54 @@ Entity& ArtificialIntelligence::getNearestEnemy() {
 
 	MobileEntity* mob;
 	list<MobileEntity*>::iterator iter;
-	for ( iter = entitiesNear.begin() ; iter != entitiesNear.end() ; ++iter ){
-			mob = *iter;
-			if(mob->getTeam() != this->entity->getTeam())
-				return *mob;
-		}
+	for (iter = entitiesNear.begin(); iter != entitiesNear.end(); ++iter) {
+		mob = *iter;
+		if (mob->getTeam() != this->entity->getTeam())
+			return *mob;
+	}
 
 	return *mob;
 }
 
 void ArtificialIntelligence::watch(MapData* mapData) {
 
-	 Coordinates coor=this->entity->getCoordinates();
+	Coordinates coor = this->entity->getCoordinates();
 
-	 Tile* tile;
-	 //Si no se esta moviendo o atacando busco a otro contrincante
-	 if(!entity->IsMoving() )
-	 {
-		 int direction = rand() % 4 ;
-		 if(direction==0)
-		 {
-			 //mobile->moveTo(coor.getCol(),coor.getRow()+WATCHSIZE);
-			 tile =  new Tile( new Coordinates(coor.getRow(),coor.getCol()+WATCHSIZE));
+	Tile* tile;
+	//Si no se esta moviendo o atacando busco a otro contrincante
+	if (!entity->IsMoving() && !entity->isDead()) {
+		int direction = rand() % 4;
+		if (direction == 0) {
+			//mobile->moveTo(coor.getCol(),coor.getRow()+WATCHSIZE);
+			tile = new Tile(
+					new Coordinates(coor.getRow(), coor.getCol() + WATCHSIZE));
 
-		 }else if(direction==1)
-		 {
-			 //mobile->moveTo(coor.getCol(),coor.getRow()-WATCHSIZE);
-			 tile =  new Tile( new Coordinates(coor.getRow(),coor.getCol()-WATCHSIZE));
-		 }else if(direction==2)
-		 {
-			 //mobile->moveTo(coor.getCol()+WATCHSIZE,coor.getRow());
-			 tile =  new Tile( new Coordinates(coor.getRow()+WATCHSIZE,coor.getCol()));
-		 }else if(direction==3)
-		 {
-			 //mobile->moveTo(coor.getCol()-WATCHSIZE,coor.getRow());
-			 tile =  new Tile( new Coordinates(coor.getRow()-WATCHSIZE,coor.getCol()));
-		 }
-		 if (tile->getCoordinates().getRow() > 0 && tile->getCoordinates().getRow() < mapData->getNRows()){
-			 if (tile->getCoordinates().getCol() > 0 && tile->getCoordinates().getCol() < mapData->getNCols()){
-				 mapData->moveMobileEntity((Mob*)this->entity,tile);
-			 }
-		 }
-		 if (tile) delete tile;
-	 }
+		} else if (direction == 1) {
+			//mobile->moveTo(coor.getCol(),coor.getRow()-WATCHSIZE);
+			tile = new Tile(
+					new Coordinates(coor.getRow(), coor.getCol() - WATCHSIZE));
+		} else if (direction == 2) {
+			//mobile->moveTo(coor.getCol()+WATCHSIZE,coor.getRow());
+			tile = new Tile(
+					new Coordinates(coor.getRow() + WATCHSIZE, coor.getCol()));
+		} else if (direction == 3) {
+			//mobile->moveTo(coor.getCol()-WATCHSIZE,coor.getRow());
+			tile = new Tile(
+					new Coordinates(coor.getRow() - WATCHSIZE, coor.getCol()));
+		}
+		if (tile->getCoordinates().getRow() > 0
+				&& tile->getCoordinates().getRow() < mapData->getNRows()) {
+			if (tile->getCoordinates().getCol() > 0
+					&& tile->getCoordinates().getCol() < mapData->getNCols()) {
+				mapData->moveMobileEntity((Mob*) this->entity, tile);
+			}
+		}
+		if (tile)
+			delete tile;
+	}
 }
 
 ArtificialIntelligence::~ArtificialIntelligence() {
 
 }
-
 
