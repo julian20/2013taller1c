1af1502f1286f4ae772b7469b07131b63144b35a
gonchub <gonchub@32cccb14-7060-b6e5-34ce-36fad607224b>
2013-06-11 16:08:57 +0000

    Gonchu: volviendo a  utilizar la clase flag. Falta redefinir el collide to con flag para que se la pueda atacar.

--- trunk/TpTaller/includes/model/entities/Flag.h
+++ trunk/TpTaller/includes/model/entities/Flag.h
@@ -8,13 +8,15 @@
 #ifndef FLAG_H_
 #define FLAG_H_
 #include <model/entities/Entity.h>
+#include <model/entities/MobileEntity.h>
 #include <model/entities/player/Player.h>
 #include <string>
 
 using namespace std;
-class Flag: public MobileEntity {
+class Flag: public Entity {
 public:
 	Flag();
+	Flag(Entity* entity);
 	void attackedBy(string player);
 	void collideTo(Player& player);
 	void receiveDamage(MobileEntity& player);
@@ -22,7 +24,7 @@ public:
 	string getClassName();
 private:
 	string player;
-	string name;
+	string classname;
 };
 
 #endif /* FLAG_H_ */
