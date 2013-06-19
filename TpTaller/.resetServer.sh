c788f4ea5b18d3006b0f81b2f25176f10ac0f4e4
lucas.006 <lucas.006@live.com.ar>
2013-06-19 16:43:17 +0000

    Lucas: el script tomando en cuenta el bindeo.

--- TpTaller/.resetServer.sh
+++ TpTaller/.resetServer.sh
@@ -3,11 +3,26 @@ port=$(sed -n "s/^.*serverPort:[ ]\([0-9][0-9]*\)/\1/p" ./configuration/server/s
 
 while true; do 
 	./TpTaller -sn
-	if [ "$?" -eq "2" ]; then 
+	result="$?"
+	if [ "$result" -eq "2" ]; then 
 		kill -9 $(lsof -n -i| grep "$port" | sed -n "s/^[a-Z]*[ ]\([0-9][0-9]*\)[ ].*$/\1/p" | grep -m 1 ".*") 2> /dev/null
-	else
+	fi
+	if [ "$result" -eq "5" ]; then
+		echo "Error al bindear al puerto, reintentando en 5 segundos"
+		sleep 1
+		echo -n "."
+		sleep 1
+		echo -n "."
+		sleep 1
+		echo -n "."
+		sleep 1
+		echo -n "."
+		sleep 1
+		echo "."
+		continue
+	fi
+	if [ "$result" -ne "2" ] && [ "$result" -ne "5" ]; then
 		exit
 	fi
 	echo "Reiniciando el servidor ..."
-	sleep 10
 done
